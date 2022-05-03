6e374fe0e0bf1280f49ef40fb9ce6486fe7ad276
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-03 14:01:33 -0700

    slice header to slice vcf

---
+++ interface/slice.h
@@ -0,0 +1,15 @@
+#ifndef SLICE_H
+#define SLICE_H
+
+#endif // SLICE_H
+
+#include <string>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+void sliceVCF(
+vector<string> splitString(string s, char delim);
+int countNumberColumns(string gwasFileName, char delim);
+vector<string> returnFileHeader(string gwasFileName, int numCols, char delim);
