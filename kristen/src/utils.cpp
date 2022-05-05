31d6367db37467d29313582b1ad2ae4ebd580d85
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-05 08:12:15 -0700

    tranpose debug/comments

--- kristen/src/utils.cpp
+++ kristen/src/utils.cpp
@@ -7,12 +7,15 @@ using namespace std;
 
 vector<vector<int>> transpose(vector<vector<int>> &b){
 	cout << "Transposing VMF to SMF..." << endl;
+	// throw error if size is bad
 	if (b.size() == 0) {
 		cerr << "Error reaching to db." << endl;
 	}
-    	vector<vector<int>> trans_vec(b[1].size(), vector<int>());
+    	
+	// transpose data
+	vector<vector<int>> trans_vec(b[1].size(), vector<int>());
     	for (size_t i = 0; i < b.size(); i++) {
-        	for (size_t j = 0; j < b[i].size(); j++) {
+		for (size_t j = 0; j < b[i].size(); j++) {
             		trans_vec[j].push_back(b[i][j]);
         	}
     	}
