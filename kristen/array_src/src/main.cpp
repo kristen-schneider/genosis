885c2c90831d794a0004a61ce423d619dea5070c
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-10 14:23:10 -0700

    changed makefile and separeted array vs vector. array still building up

---
+++ kristen/array_src/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "readVCF.h"
+
+using namespace std;
+
+//g++ main_encode.cpp slice.cpp utils.cpp -lhts -o encode
+
+int main(void){
+
+	cout << "Start of encoding." << endl;
+	
+	cout << "End of encoding." << endl;
+	return 0;
+}
---
+++ kristen/array_src/src/readVCF.h
@@ -0,0 +1,12 @@
+#ifndef READVCF_H
+#define READVCF_H
+
+#endif // READVCF_H
+
+#include <string>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+void sliceVCF(void);
