33e4d2b73cbccc2d1c034490bdd9cd3383b61b7d
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-10 13:10:32 -0700

    trying to separate vector vs array implementaion

---
+++ kristen/vec_src/simple.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "simple.h"
+
+using namespace std;
+
+int simple_print(){
+	cout << "simple test" << endl;
+	return 0;
+}
+
---
+++ kristen/vec_src/simple.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLE_H
+#define SIMPLE_H
+
+#endif //SIMPLE_H
+
+#include <iostream>
+
+using namespace std;
+
+int simple_print(void);
