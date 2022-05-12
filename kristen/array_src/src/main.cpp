821fc2a4d6cf45a6b0a6a3eac9c1d7e751e10e92
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-12 10:12:56 -0700

    trying with CMake

--- kristen/array_src/src/main.cpp
+++ kristen/array_src/src/main.cpp
@@ -11,8 +11,9 @@ using namespace std;
 int main(void){
 
 	// temp variables
-	int numSamples = 15;
-	int numVariants = 9;
+	int numSamples = 10;
+	int numVariants = 10;
+	int numQueries = 3;
 
 	cout << "Start of encoding." << endl;
 
@@ -20,10 +21,16 @@ int main(void){
 	//sliceVCF();
 
 	cout << "Reading Encoded file." << endl;
-	float* xb = read_test(numSamples, numVariants);
+	//float* xb = read_test(numSamples, numVariants);
+	
+	float seed[numVariants * numQueries] = {0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f}; 
+	float* xq = new float[numVariants * numQueries];
+	for (int i = 0; i < (numVariants * numQueries); i++) {
+        	xq[i] = seed[i];
+	}
 
 	cout << "FAISS." << endl;
-	ss(xb, numSamples, numVariants);
+	//ss(xb, xq, numSamples, numVariants, numQueries);
 
 	cout << "End of encoding." << endl;
 	return 0;
