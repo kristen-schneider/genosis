622bb62370e590499166d2acaa4858648567c5ee
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-24 12:18:35 -0700

    chunkEncodings to only read part of an encoded file as input

---
+++ include/chunkEncoding.h
@@ -0,0 +1,9 @@
+#ifndef CHUNKENCODINGS_H
+#define CHUNKENCODINGS_H
+
+#endif //CHUNKENCODINGS_H
+
+#include <iostream>
+#include <string> 
+
+float* split_encoding(string encodingtxt, int numSamples, int segmentLength);
---
+++ src/chunkEncoding.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "chunckEncoding.h"
+
+using namespace std;
+
+
