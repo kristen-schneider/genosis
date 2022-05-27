234219d971b738bfe7b7174978b6432d0acb9731
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-27 08:48:40 -0700

    reading one vector at a time and adding to index

---
+++ include/faissPM.h
@@ -0,0 +1,12 @@
+#ifndef FAISSPM_H
+#define FAISSPM_H
+
+#endif //FAISSPM_H
+
+#include <iostream>
+#include <string>
+#include <faiss/IndexFlat.h>
+
+using namespace std;
+using idx_t = faiss::Index::idx_t;
+
