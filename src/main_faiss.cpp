4e3c5c4194c480c69b403233906dc0da1333e6b4
kristen-schneider <kristen.schneider@colorado.edu>
2022-05-27 13:46:54 -0700

    faiss --> bf ... some of faiss distances non zero seem off

--- src/main_faiss.cpp
+++ src/main_faiss.cpp
@@ -3,6 +3,7 @@
 
 #include "buildIndex.h"
 #include "searchIndex.h"
+#include "bruteForce.h"
 
 using namespace std;
 // 64-bit int
@@ -17,22 +18,25 @@ int main(void){
 	int numVariants = 9;//2548903;//68819; // number of variants (cols) in encoding.txt
 	int numSamples = 2548;//2504; // number of samples (rows) in encoding.txt
 	int numQueries = 1; // number of queries
-	int segmentLength = 5;
+	int k = 7;
 
 	// path to encoded file
 	string encodingtxt = "/home/sdp/precision-medicine/data/encoded/test.encoded.txt";//ALL.wgs.svs.genotypes.encoded.txt";
 	string queriestxt = "/home/sdp/precision-medicine/data/queries/test.queries.txt";//ALL.wgs.svs.genotypes.queries.txt";
 
 	// DONE. Start FAISS..
+	cout << "---------" << endl;
 	cout << "Starting similarity searching using FAISS..." << endl;
 	
 	cout << "\n1.Builing index for " << encodingtxt << "..." << endl;
 	faiss::IndexFlatL2 index = build_faiss_index(encodingtxt, numVariants, numSamples, numQueries);
 	cout << "\n2.Running similairty search..." << endl;
-	int x = similarity_search(index, queriestxt, numVariants, numSamples, numQueries);	
+	similarity_search(index, queriestxt, numVariants, numSamples, numQueries, k);	
+	
 	cout << "End of FAISS." << endl;
+	cout << "---------" << endl;
 	cout << "Starting Brute Force." << endl;
-
+	int x = brute_force_main(encodingtxt, queriestxt, numVariants, numSamples, numQueries);
 	
 
 	return 0;
