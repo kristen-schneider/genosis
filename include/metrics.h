9a512308251a46af801bf46e8631ef1c5abb0860
kristen-schneider <kristen.schneider@colorado.edu>
2022-06-02 08:38:56 -0700

    metrics.cpp has all code for computing ddiffernt test metrics

---
+++ include/metrics.h
@@ -0,0 +1,14 @@
+#ifndef METRICS_H
+#define METRICS_H
+
+#endif //METRICS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//int brute_force_main(string eFile, string qFile, int start, int lenQuery, int numV, int numS, int numQ, int numSeg);
+//float* compute_one_query(float* q, string eFile, int start, int lenQUery, int numV, int numS, int numQ);
+float euclidean_distance(float* vec1, float* vec2, int segLength);
+float exact_match(float* vec1, float* vec2, int segLength);
---
+++ src/metrics.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <sstream>
+#include <string>
+
+#include "metrics.h"
+#include "readEncoding.h"
+
+using namespace std;
+
+float euclidean_distance(float* vec1, float* vec2, int segLength){
+
+	/*
+	for (int i = 0; i < segLength; i++)
+                cout << vec1[i] << " ";
+	cout << endl;
+	for (int i = 0; i < segLength; i++)
+                cout << vec2[i] << " ";
+	cout << endl;
+	*/
+	float eucDist = 0;
+	float sum = 0;
+	for (int i = 0; i < segLength; i++){
+		float diff = vec1[i]-vec2[i];
+		float diffSqrd = pow(diff, 2);
+		sum += diffSqrd;
+	}
+	eucDist = sqrt(sum);
+	return eucDist;
+}
+
+float exact_match(float* vec1, float* vec2, int segLength){
+
+        float numMismatches = 0;
+        for (int i = 0; i < segLength; i++){
+                if(vec1[i] != vec2[i]){
+                        numMismatches++;
+                }
+        }
+        return numMismatches;
+}
