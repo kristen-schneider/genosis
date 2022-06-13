5d73d9bb090b15722afaffd4fd6b8a3ae11532af
kristen-schneider <kristen.schneider@colorado.edu>
2022-06-13 08:09:06 -0700

    can execute normal search with bash script

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 # path to directories
 src_dir="/home/sdp/precision-medicine/src/"
+include_dir="/home/sdp/precision-medicine/include/"
+conda_dir="/home/sdp/miniconda3/envs/faiss/"
 bin_dir="/home/sdp/precision-medicine/bin/"
 data_dir="/home/sdp/precision-medicine/data/"
 
@@ -14,24 +16,38 @@ numVariants=9   # number of variants in encoded file
 numSamples=3;   # number of samples in encoded file
 numQueries=1;   # number of queries in queries file
 k=9;            # number of nearest neighbors to report
+segmentLengthStart=3;
+segmentLengthEnd=9;
+segmentLengthStep=1;
 
+source ~/miniconda3/etc/profile.d/conda.sh 
+conda activate faiss
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$conda_dir"lib"
 echo "Starting All Experiments."
-
 # for 3 differnt kinds of indexes
-for index in {0..3}
+for index in {0..0}
 do
-    echo "INDEX: " $index
+    	echo "INDEX: " $index
 
-    # for segment lengths 100-1000 (100 step)
-    for segment_length in {100..1000..100}
-    do
-        echo "SEGMENT LENGTH: $segment_length"
+    	#for segment lengths 100-1000 (100 step)
+	for segment_length in {3..9..3}
+	#for segment_length in {$segmentLengthStart..$segmentLengthEnd}
+    	do
+        	echo "SEGMENT LENGTH: " $segment_length
 
-        bin=$bin_dir"index"$index"-length"$segment_length
+        	bin=$bin_dir"index"$index"-length"$segment_length
 
-        g++ $src_dir"main.cpp" -o $bin  # compile code
-        $bin $index $segment_length     # execute binary
+        	g++ $src_dir"main_faiss.cpp" \
+			$src_dir"buildIndex.cpp" \
+			$src_dir"searchIndex.cpp" \
+			$src_dir"readEncoding.cpp" \
+			-I $include_dir \
+			-I $conda_dir"include/" \
+			-L $conda_dir"lib" \
+			-lfaiss \
+			-o $bin
+        	$bin
 
-    done
-    echo
+    	done
+   	echo
 done
