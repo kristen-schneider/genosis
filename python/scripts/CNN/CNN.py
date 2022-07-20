0d3863540b9a5966dbfec6207825611ce4b95b5d
kristen-schneider <kristen.schneider@colorado.edu>
2022-07-20 09:36:22 -0700

    moved dimension fucntion to utils

--- python/scripts/CNN/CNN.py
+++ python/scripts/CNN/CNN.py
@@ -10,27 +10,6 @@ def main():
 
 #    CNN(int(vector_length), int(num_vectors))
 #
-#def get_dimensions(CNN_input_file):
-#    vector_length = -1
-#    num_vectors = -1
-#
-#    f = open(CNN_input_file, 'r')
-#    header = f.readline()
-#    num_vectors = 0
-#    for line in f:
-#        if num_vectors == 0:
-#            d = line.strip().split()
-#            sample1_encoded = d[0]
-#            sample2_encoded = d[1]
-#            if len(sample1_encoded) != len(sample2_encoded):
-#                print('Error. Two encodings of differnt length.')
-#                return -1
-#            else:
-#                vector_length = len(sample1_encoded)
-#        num_vectors += 1
-#    f.close()
-#    return vector_length, num_vectors
-#
 #def CNN(vector_length, num_vectors):
 #    model = keras.models.Sequential()
 #    model.add(Conv1D(1, kernel_size=vector_length, input_shape=(num_vectors, 1)))
