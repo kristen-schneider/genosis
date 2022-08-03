a4ff9c24152a79f828ab90727f7b575212fee417
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-03 13:43:18 -0700

    main_encode takes arguments for input files

--- src/main_encode.cpp
+++ src/main_encode.cpp
@@ -8,10 +8,14 @@ using namespace std;
 
 // code to read VCF and write to encoded file is commented out
 // only code for reading encoded file and performing FAISS will be run
-int main(void){
+int main(int argc, char* argv[]){
+
+        // path to encoded file
+        const char *vcfFile = argv[1];        // input vcf file
+        string encodedFile = argv[2];		// output encoded file 
 	
-	const char *vcfFile = "/home/sdp/precision-medicine/data/vcf/ALL.chr14.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf";
-	string encodedFile = "/home/sdp/precision-medicine/data/encodded/ALL.chr14.encoded";
+	//const char *vcfFile = "/home/sdp/precision-medicine/data/vcf/ALL.chr14.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf";
+	//string encodedFile = "/home/sdp/precision-medicine/data/encodded/ALL.chr14.encoded";
 	//const char *vcfFile = argv[1]; 		// VCF file
 	//string encodedFile = argv[2]; 		// encoded file
 	
