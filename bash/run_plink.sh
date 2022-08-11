9e2f6dfccbbff254ae5aabd1de54445dcd9006fa
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-11 15:19:28 -0700

    running plink on all segments

--- bash/run_plink.sh
+++ bash/run_plink.sh
@@ -2,7 +2,7 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate precision-medicine
-PATH=$PATH:/plink_linux_x86_64_20220402/
+PATH=$PATH:~/plink_linux_x86_64_20220402/
 
 data_dir="/home/sdp/precision-medicine/data/segments/seg_5000/"
 cd $data_dir
