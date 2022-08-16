29fd37da9b70883c5b5f55149ba1c2def72c14a4
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-16 14:01:16 -0700

    added script to format data as cnn input

--- python/scripts/cnn/make_CNN_input.py
+++ python/scripts/cnn/make_CNN_input.py
@@ -4,12 +4,11 @@ sample_IDs_file = sys.argv[1]
 sample_encodings_file = sys.argv[2]
 plink_IBD_file = sys.argv[3]
 ID_CNN_file = sys.argv[4]
-encoding_CNN_file = sys.argv[5]
 
 def main():
     ID_list = sample_IDs_list(sample_IDs_file)
     ID_encoding_dictionary = ID_encoding_dict(ID_list, sample_encodings_file)
-    write_CNN_input(ID_encoding_dictionary, plink_IBD_file, ID_CNN_file, encoding_CNN_file)
+    write_CNN_input(ID_encoding_dictionary, plink_IBD_file, ID_CNN_file)
 
 def sample_IDs_list(sample_IDs_file):
     ID_list = []
@@ -37,15 +36,12 @@ def ID_encoding_dict(ID_list, sample_encodings_file):
 
     return ID_encoding_dictionary
 
-def write_CNN_input(ID_encoding_dictionary, plink_IBD_file, ID_CNN_file, encoding_CNN_file):
+def write_CNN_input(ID_encoding_dictionary, plink_IBD_file, ID_CNN_file):
 
     plink_f = open(plink_IBD_file, 'r')
     ID_header = 'sample1_ID\tsample2_ID\tdistance\n'
     ID_CNN_f = open(ID_CNN_file, 'w')
     ID_CNN_f.write(ID_header)
-    encoding_header = 'sample1_encoding\tsample2_encoding\tdistance\n'
-    encoding_CNN_f = open(encoding_CNN_file, 'w')
-    encoding_CNN_f.write(encoding_header)
 
     header = None
     for line in plink_f:
@@ -56,18 +52,12 @@ def write_CNN_input(ID_encoding_dictionary, plink_IBD_file, ID_CNN_file, encodin
             ID1 = A[1]
             ID2 = A[3]
             distance = float(A[11])
-
-            ID1_encoding = ID_encoding_dictionary[ID1]
-            ID2_encoding = ID_encoding_dictionary[ID2]
         
             ID_pairwise_entry = ID1 + '\t' + ID2 + '\t' + str(distance) + '\n'
             ID_CNN_f.write(ID_pairwise_entry)
-            encoding_pairwise_entry = ID1_encoding + '\t' + ID2_encoding + '\t' + str(distance) + '\n'
-            encoding_CNN_f.write(encoding_pairwise_entry)
 
     plink_f.close()
     ID_CNN_f.close()
-    encoding_CNN_f.close()
 
 
 if __name__ == '__main__':
