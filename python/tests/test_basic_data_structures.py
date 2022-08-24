0af0802e75805c16a9c7dfb91f724e7eaceb5af4
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-24 09:39:41 -0700

    adding testing dir for python

---
+++ python/tests/test_basic_data_structures.py
@@ -0,0 +1,12 @@
+# imports
+import unittest
+import sys, os, inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from scripts import basic_data_structures
+
+class TestSampleIDList():
+    
