abd53e58aa9c125680bebb50ae94a7ce48fee76f
Murad <mchowdh200@gmail.com>
2022-08-25 10:24:52 -0600

    ensure variable names

--- python/training_loop/evaluation_loop.py
+++ python/training_loop/evaluation_loop.py
@@ -24,11 +24,11 @@ if __name__ == "__main__":
     test_samples = sys.argv[2]  # 1 sample per line
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str)
-    parser.add_argument("--test_samples", type=str)
-    parser.add_argument("--sample_ids", type=str)
-    parser.add_argument("--sample_pairs", type=str)
-    parser.add_argument("--genotypes", type=str)
-    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--model_path", type=str, dest="model_path", default=model_path)
+    parser.add_argument("--test_samples", type=str, dest="test_samples")
+    parser.add_argument("--sample_ids", type=str, dest="samples_ids")
+    parser.add_argument("--sample_pairs", type=str, dest="sample_pairs")
+    parser.add_argument("--genotypes", type=str, dest="genotypes")
+    parser.add_argument("--batch_size", type=int, default=32, dest="batch_size")
     args = parser.parse_args()
     evaluate_model(args)
