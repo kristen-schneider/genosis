5cb535f2e59e73e63aec21d655e90e4ce51e1bb9
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-27 08:27:52 -0700

    removed old function

--- cpp/include/build_index.h
+++ cpp/include/build_index.h
@@ -11,11 +11,6 @@
 using namespace std;
 using idx_t = faiss::Index::idx_t;
 
-//template <class indexType>
-//indexType buildIndex(indexType index, string encodedFile, int start, int lengthSegment, int numSamples);
-
 faiss::IndexFlatL2 build_faiss_index(string encodedTXT, int num_variants, int num_samples, char delim);
 //faiss::IndexHNSWFlat build_faiss_index(string encodedTXT, int num_variants, int num_samples);
 
-//faiss::IndexFlatL2 build_faiss_index_segments(string eFile, int start, int lengthSeg, int numS);
-faiss::IndexHNSWFlat build_faiss_index_segments(string eFile, int start, int lengthSeg, int numS);
