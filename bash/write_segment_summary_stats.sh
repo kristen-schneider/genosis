ad9ccac93e9f4e9bb0f6023d9915fb25d4fe3f66
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-30 10:55:15 -0700

    separating writing and plotting scripts

--- bash/write_segment_summary_stats.sh
+++ bash/write_segment_summary_stats.sh
@@ -8,7 +8,7 @@ faiss_emb="emb_faissL2"
 k=20
 train_samples="/home/sdp/precision-medicine/data/samples/train_samples.txt"
 test_samples="/home/sdp/precision-medicine/data/samples/test_samples.txt"
-outdir="/home/sdp/precision-medicine/python/scripts/plotting/"
+out_dir="/home/sdp/precision-medicine/python/scripts/plotting/"
 num_seg=50
 
-python $python_dir/precision.py --data_dir $data_dir --plink $plink --faiss_enc $faiss_enc --faiss_emb $faiss_emb --k $k --train $train_samples --test $test_samples --outdir $outdir --num_seg $num_seg
+python $python_dir/write_stats.py --data_dir $data_dir --plink $plink --faiss_enc $faiss_enc --faiss_emb $faiss_emb --k $k --train $train_samples --test $test_samples --out_dir $out_dir --num_seg $num_seg
