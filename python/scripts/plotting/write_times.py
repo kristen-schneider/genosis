ea9c489c7aaebeb2b88094c08350e8d4e8822a53
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-30 19:45:32 -0700

    added script to wrtie times for plink and faiss for all segmetns

---
+++ python/scripts/plotting/write_times.py
@@ -0,0 +1,91 @@
+import argparse
+import re
+import sys, os
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir')
+    parser.add_argument('--plink_ext')
+    parser.add_argument('--faiss_enc_ext')
+    parser.add_argument('--faiss_emb_ext')
+    parser.add_argument('--out_dir')
+    parser.add_argument('--num_seg')
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    [plink_seg_times, faiss_enc_seg_times, faiss_emb_seg_times] = read_file_times(args)
+    write_file_times(plink_seg_times, args.out_dir + 'plink.seg.times')
+    write_file_times(faiss_enc_seg_times, args.out_dir + 'faiss_enc.seg.times')
+    write_file_times(faiss_emb_seg_times, args.out_dir + 'faiss_emb.seg.times')
+
+
+def write_file_times(times_dict, out_file):
+    o = open(out_file, 'w')
+    for s in times_dict.keys():
+        line = str(s) + '\t' + str(times_dict[s]) + '\n'
+        o.write(line)
+    o.close
+
+
+
+def read_file_times(args):
+    plink_seg_times = dict()
+    faiss_enc_seg_times = dict()
+    faiss_emb_seg_times = dict()
+
+
+    # get faiss times for each segment
+    for segment_file in os.listdir(args.data_dir):
+        f = os.path.join(args.data_dir, segment_file)
+        try: seg_i = segment_file.split('.')[2]
+        except IndexError: continue
+
+
+        # seg_i's plink file
+        if (seg_i in f and args.plink_ext in f):
+            s_time = get_plink_time(f)
+            plink_seg_times[seg_i] = s_time
+
+        # seg_i's faiss_enc file
+        elif (seg_i in f and args.faiss_enc_ext in f):
+            s_time = get_faiss_time(f)
+            faiss_enc_seg_times[seg_i] = s_time
+
+        # seg_i's faiss_emb file
+        elif (seg_i in f and args.faiss_emb_ext in f):
+            s_time = get_faiss_time(f)
+            faiss_emb_seg_times[seg_i] = s_time
+
+    return [plink_seg_times, faiss_enc_seg_times, faiss_emb_seg_times]
+
+
+def get_faiss_time(faiss_file):
+    # microseconds
+    seg_time = -1
+    f = open(faiss_file, 'r')
+    for line in f:
+        if re.search(r'^TIME', line):
+            time_line = line.split(':')
+            seg_time = int(time_line[2])
+    f.close()
+    return (seg_time / 1e6)
+
+def get_plink_time(plink_file):
+    # seconds
+    seg_time = -1
+    f = open(plink_file, 'r')
+    for line in f:
+        # Start time: Tue Aug 30 18:33:35 2022
+        if re.search(r'^Start time:', line):
+            start_time = int(line.split()[5].split(':')[-1])
+        elif re.search(r'^End time:', line):
+            end_time = int(line.split()[5].split(':')[-1])
+    seg_time = end_time - start_time
+    f.close()
+    return seg_time
+
+if __name__ == '__main__':
+    main()
+
