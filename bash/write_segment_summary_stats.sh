18855ac9f39502b7338c225486442c97eaa1ebf1
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-31 12:03:24 -0700

    added flags for more specific naming of files

--- bash/write_segment_summary_stats.sh
+++ bash/write_segment_summary_stats.sh
@@ -2,26 +2,26 @@
 
 python_dir="/home/sdp/precision-medicine/python/scripts/plotting"
 data_dir="/home/sdp/precision-medicine/data/segments/chr8/seg_5000/"
-plink_ext="genome"
-faiss_enc_ext="enc_faissHNSW"
-faiss_emb_ext="emb_faissHNSW"
-k=20
+plink_ext=".genome"
+faiss_enc_ext=".enc_faissHNSW"
+faiss_emb_ext=".emb_faissHNSW"
+faiss_idx="hnsw"
+base_name="chr8.seg."
 train_samples="/home/sdp/precision-medicine/data/samples/train_samples.txt"
 test_samples="/home/sdp/precision-medicine/data/samples/test_samples.txt"
+k=20
 out_dir="/home/sdp/precision-medicine/python/scripts/plotting/txt/"
 num_seg=50
-faiss_index='hnsw'
-
-#python $python_dir/write_stats.py --data_dir $data_dir --plink_ext $plink_ext --faiss_enc_ext $faiss_enc_ext --faiss_emb_ext $faiss_emb_ext --k $k --train $train_samples
 
 python $python_dir/write_stats.py \
 	--data_dir $data_dir \
 	--plink_ext $plink_ext \
 	--faiss_enc_ext $faiss_enc_ext \
 	--faiss_emb_ext $faiss_emb_ext \
-	--k $k \
+	--base_name $base_name \
+	--faiss_idx $faiss_idx \
 	--train $train_samples \
 	--test $test_samples \
-	--out_dir $out_dir \
+	--k $k \
 	--num_seg $num_seg \
-	--faiss_index $faiss_index
+	--out_dir $out_dir
--- python/scripts/plotting/write_stats.py
+++ python/scripts/plotting/write_stats.py
@@ -20,7 +20,7 @@ def get_args():
     parser.add_argument('--plink_ext')
     parser.add_argument('--faiss_enc_ext')
     parser.add_argument('--faiss_emb_ext')
-    parser.add_argument('faiss_idx')
+    parser.add_argument('--faiss_idx')
     parser.add_argument('--base_name')
     parser.add_argument('--train')
     parser.add_argument('--test')
@@ -107,8 +107,9 @@ def compute_segment_statistic(args, query_IDs, database_IDs, stat):
         encoding_precisions[s] = stat_encoding_seg_precision
         embedding_precisions[s] = stat_embedding_seg_precision
 
-        print('enc:TP:', stat_encoding_seg_TP, 'emb:TP:', stat_embedding_seg_TP,
-              'enc:P:', stat_encoding_seg_precision, 'emb:P:', stat_embedding_seg_precision)
+        # debugging
+        #print('enc:TP:', stat_encoding_seg_TP, 'emb:TP:', stat_embedding_seg_TP,
+        #      'enc:P:', stat_encoding_seg_precision, 'emb:P:', stat_embedding_seg_precision)
 
     stats = [encoding_true_positives, embedding_true_positives,
                encoding_precisions, embedding_precisions]
@@ -117,14 +118,14 @@ def compute_segment_statistic(args, query_IDs, database_IDs, stat):
 def write_plotting_data(args, segment_stats, stat, idx):
     [ENC_TP, EMB_TP, ENC_P, EMB_P] = segment_stats
     # write tp data to outfile
-    tp_enc_string = args.data_dir + stat + '.tp.encoding.' + idx
+    tp_enc_string = args.out_dir + stat + '.tp.encoding.' + idx
     tp_enc_data = open(tp_enc_string, 'w')
     for seg in ENC_TP.keys():
         tp_line = str(seg) + '\t' + str(ENC_TP[seg]) + '\n'
         tp_enc_data.write(tp_line)
     tp_enc_data.close()
 
-    tp_emb_string = args.data_dir + stat + '.tp.embedding' + idx
+    tp_emb_string = args.out_dir + stat + '.tp.embedding' + idx
     tp_emb_data = open(tp_emb_string, 'w')
     for seg in EMB_TP.keys():
         tp_line = str(seg) + '\t' + str(EMB_TP[seg]) + '\n'
@@ -132,14 +133,14 @@ def write_plotting_data(args, segment_stats, stat, idx):
     tp_emb_data.close()
 
     # write p data to outfile
-    p_enc_string = args.data_dir + stat + '.p.encoding.' + idx
+    p_enc_string = args.out_dir + stat + '.p.encoding.' + idx
     p_enc_data = open(p_enc_string, 'w')
     for seg in ENC_P.keys():
         p_line = str(seg) + '\t' + str(ENC_P[seg]) + '\n'
         p_enc_data.write(p_line)
     p_enc_data.close()
 
-    p_emb_string = args.data_dir + stat + '.p.embedding.' + idx
+    p_emb_string = args.out_dir + stat + '.p.embedding.' + idx
     p_emb_data = open(p_emb_string, 'w')
     for seg in EMB_P.keys():
         p_line = str(seg) + '\t' + str(EMB_P[seg]) + '\n'
