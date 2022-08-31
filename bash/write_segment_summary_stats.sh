d26691f11dd873f4457490c2227d9848da035cfb
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-30 19:45:44 -0700

    changed outdir

--- bash/write_segment_summary_stats.sh
+++ bash/write_segment_summary_stats.sh
@@ -3,14 +3,14 @@
 python_dir="/home/sdp/precision-medicine/python/scripts/plotting"
 data_dir="/home/sdp/precision-medicine/data/segments/chr8/seg_5000/"
 plink_ext="genome"
-faiss_enc_ext="enc_faissL2"
-faiss_emb_ext="emb_faissL2"
+faiss_enc_ext="enc_faissHNSW"
+faiss_emb_ext="emb_faissHNSW"
 k=20
 train_samples="/home/sdp/precision-medicine/data/samples/train_samples.txt"
 test_samples="/home/sdp/precision-medicine/data/samples/test_samples.txt"
-out_dir="/home/sdp/precision-medicine/python/scripts/plotting/"
-num_seg=50
-faiss_index='l2'
+out_dir="/home/sdp/precision-medicine/python/scripts/plotting/txt/"
+num_seg=2
+faiss_index='hnsw'
 
 #python $python_dir/write_stats.py --data_dir $data_dir --plink_ext $plink_ext --faiss_enc_ext $faiss_enc_ext --faiss_emb_ext $faiss_emb_ext --k $k --train $train_samples
 
