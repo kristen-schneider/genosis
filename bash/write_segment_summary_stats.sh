c7ae1b97f6d9755ce4448b1a4f3f24bf26284571
kristen-schneider <kristen.schneider@colorado.edu>
2022-08-30 17:45:13 -0700

    modified flag for faiss index

--- bash/write_segment_summary_stats.sh
+++ bash/write_segment_summary_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python_dir="/home/sdp/precision-medicine/python/scripts/plotting"
-data_dir="/home/sdp/precision-medicine/data/segments/chr0_test/seg_5000/"
+data_dir="/home/sdp/precision-medicine/data/segments/chr8/seg_5000/"
 plink_ext="genome"
 faiss_enc_ext="enc_faissL2"
 faiss_emb_ext="emb_faissL2"
@@ -10,14 +10,18 @@ train_samples="/home/sdp/precision-medicine/data/samples/train_samples.txt"
 test_samples="/home/sdp/precision-medicine/data/samples/test_samples.txt"
 out_dir="/home/sdp/precision-medicine/python/scripts/plotting/"
 num_seg=50
+faiss_index='l2'
+
+#python $python_dir/write_stats.py --data_dir $data_dir --plink_ext $plink_ext --faiss_enc_ext $faiss_enc_ext --faiss_emb_ext $faiss_emb_ext --k $k --train $train_samples
 
 python $python_dir/write_stats.py \
-	--data_dir $data_dir \ 
+	--data_dir $data_dir \
 	--plink_ext $plink_ext \
-       	--faiss_enc_ext $faiss_enc_ext \
+	--faiss_enc_ext $faiss_enc_ext \
 	--faiss_emb_ext $faiss_emb_ext \
 	--k $k \
 	--train $train_samples \
 	--test $test_samples \
 	--out_dir $out_dir \
 	--num_seg $num_seg \
+	--faiss_index $faiss_index
