76b4b2fa700ca0b2af79740878832f55cbdbfb18
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-04 09:25:42 -0700

    scripts for writing and plotting snp distributions

---
+++ python/scripts/plotting/plot_snp_distances.py
@@ -0,0 +1,80 @@
+import argparse
+import matplotlib.pyplot as plt
+import sys, os
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vcf')
+    parser.add_argument('--map')
+    parser.add_argument('--data_dir')
+    parser.add_argument('--out_png')
+    parser.add_argument('--num_snps')
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    bp_distance_file = args.data_dir + str(args.num_snps) + '.basepair_distance.txt'
+    bp_start_end = read_data_file(bp_distance_file)
+    cm_est_distance_file = args.data_dir + str(args.num_snps) + '.centimorgan_est_distance.txt'
+    cm_est_count = read_data_file(cm_est_distance_file)
+    cm_map_distance_file = args.data_dir + str(args.num_snps) + '.centimorgan_map_distance.txt'
+    cm_map_count = read_data_file(cm_map_distance_file)
+    plot_positions(bp_start_end, cm_est_count, cm_map_count, args.out_png, args.num_snps)
+
+def read_data_file(data_file):
+    data_dict = dict()
+    f = open(data_file, 'r')
+    for line in f:
+        L = line.strip().split(',')
+        seg = int(L[0])
+        dist = float(L[1])
+        data_dict[seg] = dist
+    f.close()
+    return data_dict
+
+def plot_positions(seg_bp, seg_cm_estimate, seg_cm_map, out_png, num_snps):
+    png_name = out_png + num_snps + '.variants.png'
+    plt.figure(figsize=(28, 15))
+    ax1 = plt.subplot(111)
+    ax1.set_title('Lengths of Segments', fontsize=30)
+
+    x = range(len(seg_bp))
+    # base pairs
+    y_bp_data = []
+    for seg in seg_bp:
+        y_bp_data.append(seg_bp[seg])
+    # cm pairs estimate
+    y_cm_estimate_data = []
+    for seg in seg_cm_estimate:
+        y_cm_estimate_data.append(seg_cm_estimate[seg])
+    # cm pairs map
+    y_cm_map_data = []
+    for seg in seg_cm_map:
+        y_cm_map_data.append(seg_cm_map[seg])
+
+    ax1.set_xticks(x)
+    bp = ax1.plot(x, y_bp_data, color='olivedrab', label='Base Pairs')
+    ax1.spines.top.set_visible(False)
+    ax1.spines.right.set_visible(False)
+    ax1.set_xlabel('Segment', fontsize=20)
+    ax1.set_ylabel('Length (bp)', fontsize=20)
+
+    ax2 = ax1.twinx()
+    cm_e = ax2.plot(x, y_cm_estimate_data, color='firebrick', label='Centimorgan Estimated')
+    ax2.set_ylabel('Length (cM)', fontsize=20)
+    ax2.spines.top.set_visible(False)
+    ax2.spines.left.set_visible(False)
+    cm_m = ax2.plot(x, y_cm_map_data, color='steelblue', label='Centimorgan Mapped')
+    ax2.spines.left.set_visible(False)
+    ax2.spines.top.set_visible(False)
+
+    lns = bp + cm_e + cm_m
+    labs = [l.get_label() for l in lns]
+    ax2.legend(lns, labs, fontsize=40)
+
+    plt.savefig(png_name)
+
+if __name__ == '__main__':
+    main()
---
+++ python/scripts/plotting/write_snp_distances.py
@@ -0,0 +1,104 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vcf')
+    parser.add_argument('--map')
+    parser.add_argument('--out_data')
+    parser.add_argument('--num_snps')
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    bp_start_end = dict()
+    cm_map_dict = dict()
+    cm_est_count = dict()
+    cm_map_count = dict()
+
+    bp_start_end = get_all_bp_count(args.vcf, int(args.num_snps))
+    cm_map_dict = get_cm_map(args.map)
+    cm_est_count = get_segment_cm_estimated(bp_start_end)
+    cm_map_count = get_segment_cm_map(bp_start_end, cm_map_dict)
+
+    bp_distance_file = args.out_data + str(args.num_snps) + '.basepair_distance.txt'
+    write_snp_data(bp_start_end, bp_distance_file)
+    cm_est_distance_file = args.out_data + str(args.num_snps) + '.centimorgan_est_distance.txt'
+    write_snp_data(cm_est_count, cm_est_distance_file)
+    cm_map_distance_file = args.out_data + str(args.num_snps) + '.centimorgan_map_distance.txt'
+    write_snp_data(cm_map_count, cm_map_distance_file)
+
+def write_snp_data(snp_data, out_file):
+    f = open(out_file, 'w')
+    for seg in snp_data:
+        dist = snp_data[seg]
+        if isinstance(dist, list):
+            dist = dist[1] - dist[0]
+        line = str(seg) + ',' + str(dist) + '\n'
+        f.write(line)
+    f.close()
+
+def get_all_bp_count(vcf_file, num_snps):
+    bp_start_end = dict()
+    seg_i = 0
+    seg_snp_count = 0
+    seg_positions = []
+    f = open(vcf_file)
+    for line in f:
+        if '#' in line:
+            continue
+        elif (seg_snp_count < num_snps):
+            pos = int(line.strip().split()[1])
+            seg_positions.append(pos)
+            seg_snp_count += 1
+        else:
+            # save off last segment
+            bp_start_end[seg_i] = [seg_positions[0], seg_positions[-1]]
+            seg_i += 1
+            # start new segment
+            pos = int(line.strip().split()[1])
+            seg_positions = [pos]
+            seg_snp_count = 1
+    f.close()
+    # anything leftover
+    if (seg_snp_count <= num_snps):
+        seg_i += 1
+        bp_start_end[seg_i] = [seg_positions[0], seg_positions[-1]]
+
+    return bp_start_end
+
+def get_cm_map(interpolated_map):
+    pos_cm_dict = dict()
+    f = open(interpolated_map, 'r')
+    for line in f:
+        L = line.strip().split()
+        cm = L[2]
+        pos = L[3]
+        pos_cm_dict[int(pos)] = float(cm)
+    f.close()
+    return pos_cm_dict
+
+def get_segment_cm_estimated(bp_start_end):
+    # 1e6 bp = 1cM
+    cm_est_count = dict()
+    for seg in bp_start_end:
+        bp_distance = bp_start_end[seg][1] - bp_start_end[seg][0]
+        bp_per_cm = 1e6
+        cm_estimate = bp_distance / bp_per_cm
+        cm_est_count[seg] = cm_estimate
+    return cm_est_count
+
+def get_segment_cm_map(bp_start_end, cm_map_dict):
+    # cM distance from map file
+    cm_map_count = dict()
+    for seg in bp_start_end:
+        startPOS = bp_start_end[seg][0]
+        endPOS = bp_start_end[seg][1]
+        startCM = cm_map_dict[startPOS]
+        endCM = cm_map_dict[endPOS]
+        cm_map = endCM-startCM
+        cm_map_count[seg] = cm_map
+    return cm_map_count
+
+if __name__ == '__main__':
+    main()
