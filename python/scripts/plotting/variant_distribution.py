aa4d091b188e7c3b480abf7638b5a739cf04301f
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-03 20:05:12 -0700

    plotting bp vs cm

--- python/scripts/plotting/variant_distribution.py
+++ python/scripts/plotting/variant_distribution.py
@@ -7,12 +7,13 @@ def get_args():
     parser.add_argument('--vcf')
     parser.add_argument('--map')
     parser.add_argument('--out_png')
+    parser.add_argument('--num_vars')
 
     return parser.parse_args()
 
 def main():
     args = get_args()
-    bp = get_bps(args.vcf, 555500)
+    bp = get_bps(args.vcf, int(args.num_vars))
     plot_hist(bp, args.out_png)
 
 def get_bps(vcf_file, num_variants):
@@ -35,7 +36,7 @@ def plot_hist(bps, outpng):
     plt.figure(figsize=(28, 15))
     ax1 = plt.subplot(111)
     ax1.set_title('distribution of variants', fontsize=30)
-    ax1.hist(bps, color='olivedrab')
+    ax1.hist(bps, bins=100, color='olivedrab')
     ax1.set_xlabel('basepair position', fontsize=20)
     ax1.spines.right.set_visible(False)
     ax1.spines.top.set_visible(False)
