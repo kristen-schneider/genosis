9ab9f00363b8ff093885d5285ae59cb1dc66c885
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-06 18:28:25 -0700

    editing run plink to run on many vcf files for ibd segments

--- bash/run_plink.sh
+++ bash/run_plink.sh
@@ -1,18 +1,16 @@
 #!bin/sh
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate precision-medicine
 PATH=$PATH:~/plink_linux_x86_64_20220402/
 
-data_dir="/home/sdp/precision-medicine/data/segments/chr0-test/seg_3/"
+data_dir="/home/sdp/precision-medicine/data/ibd/iLASH/"
 cd $data_dir
 
 for vcf in $data_dir*.vcf;
 do
 	echo $vcf
-	plink --vcf $vcf --genome
 	filename=$(basename $vcf)
 	seg_name=${filename%.*}
-	mv $data_dir"plink.genome" $data_dir$seg_name".genome"
+	echo $seg_name
+	#plink --vcf $vcf --genome --out $seg_name
 done
 
