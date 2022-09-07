0f1e07bfd3d843f4ee2569319afa1e6adca5761b
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-07 09:42:25 -0700

    madde changes to pass bp starta nd endd to python script

--- bash/ilash_to_vcf.sh
+++ bash/ilash_to_vcf.sh
@@ -2,17 +2,32 @@
 
 PATH=$PATH:~/plink_linux_x86_64_20220402/
 
+vcf_file="/home/sdp/precision-medicine/data/vcf/ALL.chr8.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf"
 python_dir="/home/sdp/precision-medicine/python/scripts/ibd/"
 ilash_file="/home/sdp/precision-medicine/data/ibd/iLASH/ALL.chr8.ilash.match"
-test_file="/home/sdp/precision-medicine/data/ibd/iLASH/test.test"
 data_dir="/home/sdp/precision-medicine/data/ibd/iLASH/"
 cd $data_dir
 
 i=0
+echo "iLASH"
 while read -r F1 S1 F2 S2 chr bp_start bp_end snp_s snp_e cm percent;
 do
-	echo "start: " $bp_start
-	echo $bp_end
+
+	# make vcf from start and end of cm
+	echo "Creating ibd segment vcf..."
+	out_vcf=$data_dir"ibd.$i.vcf"
+	python $python_dir"ilash_to_vcf.py" --vcf $vcf_file --start $bp_start --end $bp_end --out_vcf $out_vcf
+
+	# run plink on vcf
+	echo "Running plink on ibd segment vcf..."
+	plink --vcf $out_vcf --genome --out "ibd.$i"
+	
+	# delete vcf
+	echo "Removing ibd segment vcf..."
+
+
+	i=$((i + 1))
+
 done < $ilash_file
 
 #while read line; do
--- python/scripts/ibd/ilash_to_vcf.py
+++ python/scripts/ibd/ilash_to_vcf.py
@@ -5,18 +5,19 @@ def get_args():
     parser.add_argument('--vcf')
     parser.add_argument('--start')
     parser.add_argument('--end')
-    parser.add_argument('--out_dir')
+    parser.add_argument('--out_vcf')
     return parser.parse_args()
 
 def main():
     args = get_args()
-    for ibd_match in segment_boundaries:
-        [ibd_start, ibd_end] = segment_boundaries[ibd_match]
-        out_vcf_name = args.out_dir + 'ibd.' + str(ibd_match) + '.vcf'
-        write_vcf_segment(args.vcf, ibd_start, ibd_end, out_vcf_name)
+    write_vcf_segment(args.vcf, int(args.start), int(args.end), args.out_vcf)
+    #for ibd_match in segment_boundaries:
+    #    [ibd_start, ibd_end] = segment_boundaries[ibd_match]
+    #    out_vcf_name = args.out_dir + 'ibd.' + str(ibd_match) + '.vcf'
+    #    write_vcf_segment(args.vcf, ibd_start, ibd_end, out_vcf_name)
 
 def write_vcf_segment(vcf_file, start, end, out_vcf):
-    print(vcf_file)
+    print('writing to ', out_vcf)
     o = open(out_vcf, 'w')
     f = open(vcf_file, 'r')
     for line in f:
@@ -29,8 +30,7 @@ def write_vcf_segment(vcf_file, start, end, out_vcf):
             o.write(line)
     f.close()
     o.close()
-
-
+    print('done writint to ', out_vcf)
 
 if __name__ == '__main__':
     main()
