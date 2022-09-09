a7bfc7b51c2ecd563f205c8a968d759945da1205
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-09 09:49:30 -0700

    reads record and prints sample GT

--- python/scripts/pysam/count_sample_snps.py
+++ python/scripts/pysam/count_sample_snps.py
@@ -15,11 +15,13 @@ def main():
     chrm = args.chrm
     cm_endpoint_file = args.cm_endpoints
     
-    seg_endpoint_dict = read_vcf.read_endpoint_file(cm_endpoint_file)
-    for seg in seg_endpoint_dict:
-        start_bp = seg_endpoint_dict[seg][0]
-        end_bp = seg_endpoint_dict[seg][1]
-        sample_snp_counts = read_vcf.sample_counts_from_vcf_block(vcf_file, chrm, start_bp, end_bp) 
+    #seg_endpoint_dict = read_vcf.read_endpoint_file(cm_endpoint_file)
+    #for seg in seg_endpoint_dict:
+    #    start_bp = seg_endpoint_dict[seg][0]
+    #    end_bp = seg_endpoint_dict[seg][1]
+    #    sample_snp_counts = read_vcf.sample_counts_from_vcf_block(vcf_file, chrm, start_bp, end_bp) 
+
+    read_vcf.sample_counts_from_vcf_block(vcf_file, chrm, 100, 61600) 
 
 if __name__ == '__main__':
     main()
