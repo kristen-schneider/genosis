ea9afdc1604adc30de6c0e6081e29b7124d717c9
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-08 18:53:47 -0700

    two scropts to fond bp endpoitsnf ro segmetns defined by different legnths

--- python/scripts/pysam/find_seg_endpoints_snp.py
+++ python/scripts/pysam/find_seg_endpoints_snp.py
@@ -13,10 +13,10 @@ def main():
     map_file = args.map
     segment_snp_length = int(args.snps)
     
-    print('seg', 'start, end')
-    segment_endpoints_dict = read_map.find_segment_bp_endpoints(map_file, segment_snp_length)    
-    for seg in segment_endpoints_dict:
-        print(seg, segment_endpoints_dict[seg])
+    print('seg', 'start_bp, end_bp')
+    snp_segment_endpointsdict = read_map.find_segment_bp_endpoints_snp(map_file, segment_snp_length)    
+    for seg in snp_segment_endpoints_dict:
+        print(seg, snp_segment_endpoints_dict[seg])
 
 if __name__ == '__main__':
     main()
