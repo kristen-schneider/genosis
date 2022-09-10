cba64d750f96fde1f8cc8cc870fb760a3a281994
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-09 17:14:15 -0700

    changing directory names for 30x

--- bash/run_genetic_distances.py
+++ bash/run_genetic_distances.py
@@ -6,37 +6,37 @@ map_file='/home/sdp/precision-medicine/data/maps/ALL.chr8.interpolated.map'
 snp_segment_sizes=(1000 5000 10000 50000 100000 1000000)
 cm_segment_sizes=(1 2 3)
 
-out_dir='/home/sdp/precision-medicine/data/snps'
+out_dir='/home/sdp/precision-medicine/data/snps/chr8-30x'
 chr=8
 
 
-echo "Measuring centimorgan lengths for varying segment sizes (SNPs)."
-for i in "${snp_segment_sizes[@]}"
-do
-	echo "...segment size = $i SNPs"
-	out_file="$out_dir/chr$chr.seg-cm.snp_size.$i"
-	echo "...writing to: $out_file"
-	python $python_dir/"count_seg_bp.py" --map $map_file --snps $i > $out_file
-done
-
-echo "Finding bp endpoints for varying segment sizes (SNPs)."
-for i in "${snp_segment_sizes[@]}"
-do
-    echo "...segment size = $i SNPs"
-    out_file="$out_dir/chr$chr.endpoints.snp_size.$i"
-    echo "...writing to: $out_file"
-    python $python_dir/"find_seg_endpoints_snp.py" --map $map_file --snps $i > $out_file
-done
-
-
-echo "Finding bp endpoints for varying segment sizes (cM)."
-for i in "${cm_segment_sizes[@]}"
-do
-    echo "...segment size = $i cMs"
-    out_file="$out_dir/chr$chr.endpoints.cm_size.$i"
-    echo "...writing to: $out_file"
-    python $python_dir/"find_seg_endpoints_cm.py" --map $map_file --cm_max $i > $out_file
-done
+#echo "Measuring centimorgan lengths for varying segment sizes (SNPs)."
+#for i in "${snp_segment_sizes[@]}"
+#do
+#	echo "...segment size = $i SNPs"
+#	out_file="$out_dir/chr$chr.seg-cm.snp_size.$i"
+#	echo "...writing to: $out_file"
+#	python $python_dir/"count_seg_bp.py" --map $map_file --snps $i > $out_file
+#done
+#
+#echo "Finding bp endpoints for varying segment sizes (SNPs)."
+#for i in "${snp_segment_sizes[@]}"
+#do
+#    echo "...segment size = $i SNPs"
+#    out_file="$out_dir/chr$chr.endpoints.snp_size.$i"
+#    echo "...writing to: $out_file"
+#    python $python_dir/"find_seg_endpoints_snp.py" --map $map_file --snps $i > $out_file
+#done
+#
+#
+#echo "Finding bp endpoints for varying segment sizes (cM)."
+#for i in "${cm_segment_sizes[@]}"
+#do
+#    echo "...segment size = $i cMs"
+#    out_file="$out_dir/chr$chr.endpoints.cm_size.$i"
+#    echo "...writing to: $out_file"
+#    python $python_dir/"find_seg_endpoints_cm.py" --map $map_file --cm_max $i > $out_file
+#done
 
 echo "Counting number of SNPs in each segment for vary segment sizes (cM)."
 for i in "${cm_segment_sizes[@]}"
