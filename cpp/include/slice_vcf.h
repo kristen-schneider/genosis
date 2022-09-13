de8cd341c280905320d93f339c8e1364afea8cab
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-13 15:52:36 -0700

    slicing by cm distance

--- cpp/include/slice_vcf.h
+++ cpp/include/slice_vcf.h
@@ -11,5 +11,6 @@
 using namespace std;
 
 vector<string> read_vcf_header(string vcf_file);
-int slice(string vcf_file, vector<string> vcf_header, int segment_size, string base_name, string out_dir);
-
+int slice(string vcf_file, vector<string> vcf_header, vector<int> segment_SNP_counts, string base_name, string out_dir);
+vector<int> read_map_file(string map_file, float slice_size);
+void split_line(const string &s, char delim, vector<string> &elems);
