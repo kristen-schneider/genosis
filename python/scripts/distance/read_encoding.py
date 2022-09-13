0893b241c018e24bdcd4579b7954ff5f673490d6
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-13 14:42:51 -0700

    reading in as dict for sample id

--- python/scripts/distance/read_encoding.py
+++ python/scripts/distance/read_encoding.py
@@ -1,9 +1,9 @@
 def read_encoding_file(encoding_file):
-    encodings = []
+    encodings = dict()
     f = open(encoding_file, 'r')
     for line in f:
         L = line.strip().split()
         sample_ID = L[0]
-        encodings.append([float(i) for i in L[1:]])
+        encodings[sample_ID] = [float(i) for i in L[1:]]
     f.close()
     return encodings
