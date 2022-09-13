39355355b6c53ea611c984d473ac9d777d74deea
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-13 14:33:00 -0700

    added calls to each metric

--- python/scripts/distance/compare_distances.py
+++ python/scripts/distance/compare_distances.py
@@ -1,6 +1,6 @@
 import argparse
 import distance_calculations
-import read_encoding_file
+import read_encoding
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -9,10 +9,30 @@ def get_args():
 
 def main():
     args = get_args()
-    encodings = read_encoding_file.read_encoding_file(args.encoding_file)
+    encodings = read_encoding.read_encoding_file(args.encoding_file)
 
-    Query = encodings[0]
-    Database = encodings
+    query = encodings[0]
+    print(query)
+    database = encodings
+    
+    # hamming distance
+    hamming_distances = []
+    for v_i in database:
+        v_hd = distance_calculations.hamming_distance(query, v_i)
+        hamming_distances.append(v_hd)
+
+    # normalized hamming distance
+    norm_hamming_distances = []
+    for hd in hamming_distances:
+        n_hd = hd/len(query)
+        norm_hamming_distances.append(n_hd)
+
+    # euclidean distance
+    euclidean_distances = []
+    for v_i in database:
+        v_ed = distance_calculations.euclidean_distance(query, v_i)
+        euclidean_distances.append(v_ed)
+
 
 
 if __name__ == '__main__':
