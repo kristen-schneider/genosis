baaa0acbde0e24b01031f8ebe31c5f636ce9a629
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-13 14:33:57 -0700

    ignoring sampleID column and reading into floats

--- python/scripts/distance/read_encoding.py
+++ python/scripts/distance/read_encoding.py
@@ -1,8 +1,9 @@
 def read_encoding_file(encoding_file):
     encodings = []
-    f = open(encoding_file):
+    f = open(encoding_file, 'r')
     for line in f:
         L = line.strip().split()
-        encodings.append(L)
+        sample_ID = L[0]
+        encodings.append([float(i) for i in L[1:]])
     f.close()
     return encodings
