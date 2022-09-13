4d191164f29bc4f9b073e935900405ed5e0babd5
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-13 14:42:24 -0700

    making all metrics dictionaries to keep sampleid

--- python/scripts/distance/compare_distances.py
+++ python/scripts/distance/compare_distances.py
@@ -5,33 +5,33 @@ import read_encoding
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--encoding_file')
+    parser.add_argument('--query')
     return parser.parse_args()
 
 def main():
     args = get_args()
     encodings = read_encoding.read_encoding_file(args.encoding_file)
 
-    query = encodings[0]
-    print(query)
+    query = encodings[args.query]
     database = encodings
     
     # hamming distance
-    hamming_distances = []
+    hamming_distances = dict()
     for v_i in database:
-        v_hd = distance_calculations.hamming_distance(query, v_i)
-        hamming_distances.append(v_hd)
+        v_hd = distance_calculations.hamming_distance(query, database[v_i])
+        hamming_distances[v_i] = v_hd
 
     # normalized hamming distance
-    norm_hamming_distances = []
+    norm_hamming_distances = dict()
     for hd in hamming_distances:
-        n_hd = hd/len(query)
-        norm_hamming_distances.append(n_hd)
+        n_hd = hamming_distances[hd]/len(query)
+        norm_hamming_distances[hd] = n_hd
 
     # euclidean distance
-    euclidean_distances = []
+    euclidean_distances = dict()
     for v_i in database:
-        v_ed = distance_calculations.euclidean_distance(query, v_i)
-        euclidean_distances.append(v_ed)
+        v_ed = distance_calculations.euclidean_distance(query, database[v_i])
+        euclidean_distances[v_i] = v_ed
 
 
 
