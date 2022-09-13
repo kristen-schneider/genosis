fd10375cfeb420c5816530b23e257fe72d1bbcb3
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-13 16:20:06 -0700

    new biallelic encoding

--- cpp/src/map_encodings.cpp
+++ cpp/src/map_encodings.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 
 #include "map_encodings.h"
 
@@ -50,4 +51,66 @@ map<string,int> make_encoding_map(string encodingFile){
 
    	}
    	return encodingMap;
+}
+
+map<string,vector<int>> make_biallelic_encoding_map(string encodingFile){
+	 /*
+         * read a file with genotype and its ecoding int
+         * a map where the key is the genotype and the 
+         * value is the bi-allelc encoding for that genotype
+         */
+
+        // a map whose key is the genotype and whose value is the encoding
+        map<string, int> encodingMap;
+
+	// a biallelic encoding is two bits
+	vector<int> biallelic_encoding;
+
+        // delimiter separates key and value
+        string delim = " ";
+
+        // open file with encoding specifications
+        ifstream eFile;
+
+        eFile.open(encodingFile);
+        if ( !eFile.is_open() ) {
+            cout << "Failed to open: " << encodingFile << endl;
+        }
+
+	// read file
+        if(eFile.is_open()){
+            string line;
+	    char delim = ' ';
+	    vector<string> vec_line;
+            string genotype;
+
+            // split by delimiter
+            while(getline(eFile, line)){
+		split_line(line, delim, vec_line);
+            	vector<int> g_encoding;
+
+		genotype = vec_line[0];
+		g_encoding.push_back(vec_line[1]);
+		g_encoding.push_back(vec_line[2]);
+
+                // add genotype encoding to map
+                pair<string, vector<int>> p (genotype, g_encoding);
+                encodingMap.insert(p);
+            }
+
+        }
+        return encodingMap;
+
+}
+
+/*
+ * Splits a string by some delimiter
+ */
+void split_line(const string &s, char delim, vector<string> &elems){
+    stringstream ss;
+    ss.str(s);
+    string item;
+    while (getline(ss, item, delim)) {
+            elems.push_back(item);
+    }
 }
