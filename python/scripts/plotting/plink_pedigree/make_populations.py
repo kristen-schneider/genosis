0cde8b55766d63d57f362f4fc6a802a7601b5382
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-14 15:41:06 -0700

    plotting violin plots for different query populations

--- python/scripts/plotting/plink_pedigree/make_populations.py
+++ python/scripts/plotting/plink_pedigree/make_populations.py
@@ -8,7 +8,28 @@ def get_args():
 
 def main():
     args = get_args()
-    pop_samples = make_pop_samples_dict(args.sample_data)
+    make_pop_summary_file(args.sample_data, args.samples_dir)
+    #pop_samples = make_pop_samples_dict(args.sample_data)
+
+def make_pop_summary_file(sample_metadata, out_dir):
+    o = open(out_dir + 'population.summary', 'w')
+
+    f = open(sample_metadata, 'r')
+    header = f.readline()
+    for line in f:
+        L = line.strip().split('\t')
+        sample_ID = L[0]
+        pop_code = L[3]
+        pop_name = L[4]
+        super_pop_code = L[5]
+        super_pop_name = L[6]
+        
+        out_line = sample_ID + ',' + pop_code + ',' + pop_name + ',' + super_pop_code + ',' + super_pop_name + '\n'
+        o.write(out_line)
+    f.close()
+    o.close()
+
+def write_pop_samples(pop_samples):
     pop_codes = pop_samples.keys()
 
     for pc in pop_codes:
@@ -17,8 +38,6 @@ def main():
         for s in pop_samples[pc]:
             o.write(s + '\n')
         o.close()
-
-
 
 def make_pop_samples_dict(sample_metadata): 
     pop_samples = dict()
