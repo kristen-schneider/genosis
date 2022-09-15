64cb30f8a51a64d75c439fb6c0340a1b3991eca1
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-15 07:24:41 -0700

    changes to plot format

--- python/scripts/distance/plot_EDHD.py
+++ python/scripts/distance/plot_EDHD.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser = argparse.add_argument('--ed')
-    parser = argparse.add_argument('--hd')
-    parser = argparse.add_argument('--png')
+    parser.add_argument('--ed')
+    parser.add_argument('--hd')
+    parser.add_argument('--png')
     return parser.parse_args()
 
 def main():
@@ -13,7 +13,6 @@ def main():
     ed_dict = read_distance_file(args.ed)
     hd_dict = read_distance_file(args.hd)
     plot_ed_hd(ed_dict, hd_dict, args.png)
-
 
 def plot_ed_hd(ed_dict, hd_dict, png):
     # format data
@@ -24,11 +23,11 @@ def plot_ed_hd(ed_dict, hd_dict, png):
         hd_data.append(hd_dict[sample_h])
 
     # plot data
-    #plt.figure(figsize=(28, 15))
+    plt.figure(figsize=(10, 10))
     ax1 = plt.subplot(111)
-    png_title = 'Euclidean distance vs Hamming distance\n'
-                'for binary haolotype encodings\n' \
-                'on 1 cm segments\n (Segmnet 0, CHR 8)'
+    png_title = 'Euclidean distance vs Hamming distance\n' \
+                'for binary haplotype encodings\n' \
+                'on 1 cm segments for 1 query\n (Segment 0, CHR 8)'
     ax1.set_title(png_title)
     ax1.scatter(ed_data, hd_data, color='olivedrab')
     # format plot
@@ -38,7 +37,6 @@ def plot_ed_hd(ed_dict, hd_dict, png):
     ax1.spines.top.set_visible(False)
     ax1.spines.right.set_visible(False)
     plt.savefig(png_name)
-    
 
 def read_distance_file(distance_file):
     sample_ID_dist_dict = dict()
@@ -47,10 +45,10 @@ def read_distance_file(distance_file):
     query = f.readline().strip().split()[1]
     header = f.readline()
     for line in f:
-        L = line.strip.split()
+        L = line.strip().split()
         sample_ID = L[0]
         dist = float(L[1])
-        sample_ID_dict[sample_ID] = dist
+        sample_ID_dist_dict[sample_ID] = dist
     f.close()
 
     return sample_ID_dist_dict
