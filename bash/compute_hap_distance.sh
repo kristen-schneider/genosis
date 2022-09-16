baf2a8632eff17bf6a144380f80660b88ff17caf
kristen-schneider <kristen.schneider@colorado.edu>
2022-09-16 13:03:34 -0700

    computing new distance metric for all haps for all segments

---
+++ bash/compute_hap_distance.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+python_dir='/home/sdp/precision-medicine/python/scripts/distance/'
+data_dir='/home/sdp/precision-medicine/data/segments/chr8-30x/'
+
+sampleID='HG00405'
+paternalID='HG00403'
+maternalID='HG00404'
+haps=(0 1)
+
+for encoded_f in $data_dir*.encoded
+do
+	filename=$(basename -- $encoded_f)
+        base=${filename%.*}
+	echo "Computing distances for " $filename
+	
+	for h in "${haps[@]}"
+	do
+		echo "...haplotype" $h
+        	kdist_f=$data_dir$base"hap."$h".kdist"
+
+		python $python_dir"compute_trios.py" \
+			--hap $h \
+			--query $sampleID \
+			> $kdist_f
+	done
+done
+
