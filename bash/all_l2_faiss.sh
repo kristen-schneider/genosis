aea5a54f41db220ee31618a57f3ffe87d57fe35b
kristen-schneider <kristen.schneider@colorado.edu>
2022-10-11 11:50:05 -0700

    faiss l2 scripts for all files

--- bash/all_l2_faiss.sh
+++ bash/all_l2_faiss.sh
@@ -2,7 +2,6 @@
 
 # to run a single faiss search on a input encoding vectors
 
-
 # path to directories
 src_dir="/home/sdp/precision-medicine/cpp/src/"
 include_dir="/home/sdp/precision-medicine/cpp/include/"
@@ -13,8 +12,8 @@ data_dir="/home/sdp/precision-medicine/data/"
 # path to encoded and query file
 test_samples=$data_dir"samples/chr8-30x/testing.samples"
 train_samples=$data_dir"samples/chr8-30x/training.samples"
-encoded_file=$data_dir"segments/chr8-30x/chr8-30x.seg.0.encoded"
-k=32 # number of nearest neighbors to report
+#encoded_file=$data_dir"segments/chr8-30x/chr8-30x.seg.0.encoded"
+k=100 # number of nearest neighbors to report
 delim="space"
 
 source ~/miniconda3/etc/profile.d/mamba.sh 
@@ -38,5 +37,13 @@ g++ $src_dir"faiss_l2.cpp" \
 	-o $bin
 
 echo "Executing..."
-
-$bin $train_samples $encoded_file $test_samples $encoded_file $k $delim
+#$bin $train_samples $encoded_file $test_samples $encoded_file $k $delim
+for encoded_f in $data_dir"segments/chr8-30x/"*.encoded
+do
+	filename=$(basename -- $encoded_f)
+        base=${filename%.*}
+	echo "Running FAISS on" $filename
+	faiss_out=$data_dir"segments/chr8-30x/"$base".enc.faissl2"
+	#echo $faiss_out
+	$bin $train_samples $encoded_f $test_samples $encoded_f $k $delim > $faiss_out
+done
