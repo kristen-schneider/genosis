8ac209d6488e55877f7e0f91ae920e007219cfdf
kristen-schneider <kristen.schneider@colorado.edu>
2022-11-11 08:42:50 -0800

    modified workflow of training loop

--- python/transformer_model/training_loop.py
+++ python/transformer_model/training_loop.py
@@ -21,40 +21,42 @@ def get_args():
 def main():
     args = get_args()
 
-    print('-Loading sample names...')
+    # loading sample names (sample_name: sample_index)
     sample_names_index = map_sample_names_to_index(args.sample_names)
-    print('-Loading genotype encodings...')
+    # loading genotype encodings (sample_index: genotype_encoding)
     genotype_encodings_index = map_genotype_encoding_to_index(args.sample_encodings)
-    print('-Splitting samples into training, testing, and validating set...')
+    # splitting IDs into training, testing, and validating sets
     training_IDs, testing_IDs, validating_IDs = split_samples(args.sample_names, .8)
 
+    # TODO: check these variables for correctness
     sequence_length = len(genotype_encodings_index[0])
     vocab_size = 4
     output_length = sequence_length
-    print('-Converting genotypes to tensors...')
+    # converting genotypes to tensors
     gt_tensors = get_genotype_tensors(sample_names_index,
                        genotype_encodings_index,
                        training_IDs)
-    print('-Converting tensors to fixed weight embeddings...')
+    # converting tensors to fixed weight embeddings for input to positional encoding
     fixed_weights_embedding_layer = PositionEmbeddingFixedWeights(sequence_length,
                                                                   vocab_size,
                                                                   output_length)
     fixed_embedding = fixed_weights_embedding_layer(gt_tensors)
-    print('-Testing encoder...')
 
+    # TODO: check these variables for correctness
     h = 8  # Number of self-attention heads
     d_k = 64  # Dimensionality of the linearly projected queries and keys
     d_v = 64  # Dimensionality of the linearly projected values
     d_model = 512  # Dimensionality of the model sub-layers' outputs
     batch_size = 64  # Batch size from the training process
-
+    # TODO: change these to match data from genotype tensors
     queries = np.random.random((batch_size, sequence_length, d_k))
     keys = np.random.random((batch_size, sequence_length, d_k))
     values = np.random.random((batch_size, sequence_length, d_v))
 
+    # mulit-head attention
     multihead_attention = MultiHeadAttention(h, d_k, d_v, d_model)
     print(multihead_attention(queries, keys, values))
-
+    # TODO: check these variables for correctness
     n = 6  # Number of layers in the encoder stack
     d_ff = 2048  # Dimensionality of the inner fully connected layer
     dropout_rate = 0.1  # Frequency of dropping the input units in the dropout layers
