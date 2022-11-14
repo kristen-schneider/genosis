1e81720a6e126a61945765666c0933659eec3879
kristen-schneider <kristen.schneider@colorado.edu>
2022-11-14 10:02:15 -0800

    workflow up to sliceing

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -4,9 +4,9 @@
 #include <vector>
 
 //#include "encode_vcf.h"
-//#include "map_encodings.h"
+#include "map_encodings.h"
 #include "read_config.h"
-//#include "slice_vcf.h"
+#include "slice_vcf.h"
 
 using namespace std;
 
@@ -16,26 +16,30 @@ int main(int argc, char* argv[]){
     	*/
 		
 	// read config file
-	cout << "Reading Config File..." << endl;
+	cout << "Loading Config Options..." << endl;
 	string configFile = argv[1];   // configuration file will all options
 	map<string, string> config_options;
-	config_options = get_config_options(configFile);
-		
+	config_options = get_config_options(configFile);		
 	// access each option by variable name
-	string encoding_file = config_options["encoding_file"];
 	string vcf_file = config_options["vcf_file"];
-	int segment_size = stoi(config_options["segment_size"]);
+	string map_file = config_options["map_file"];
+	string encoding_file = config_options["encoding_file"];
+	string query_file = config_options["query_file"];
 	string out_dir = config_options["out_dir"];
-    	string output_base_name = config_options["out_base_name"];
-	cout << encoding_file << segment_size << endl;
-	/*
+    	string out_base_name = config_options["out_base_name"];
+	int segment_size = stoi(config_options["segment_size"]);
+	cout << segment_size << endl;
+	
 	// make encoding map
+	cout << "Loading Encoding Map..." << endl;
 	map<string, int> encoding_map = make_encoding_map(encoding_file);
-
+	
 	// slice vcf into segments
 	cout << "Slicing VCF..." << endl;
-	int num_segments = slice(vcf_file, segment_size, output_base_name, out_dir);
+	int slicem = slice_main(map_file, segment_size, vcf_file, out_base_name, out_dir);
+	//int num_segments = slice(vcf_file, segment_size, output_base_name, out_dir);
 
+	/*
 	// encoded vcf segments 
 	cout << "Encoding VCF..." << endl;
 	write_all_segments(num_segments, encoding_map, out_dir, output_base_name);
