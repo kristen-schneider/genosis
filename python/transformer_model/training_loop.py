c090634a450ca909ab926e9dba0ff1db5f3a1f24
kristen-schneider <kristen.schneider@colorado.edu>
2022-11-14 18:11:36 -0800

    adding model

--- python/transformer_model/training_loop.py
+++ python/transformer_model/training_loop.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 #from decoder import Decoder
 #from encoder import Encoder
 #from positional_encoding import PositionEmbeddingFixedWeights
-from models import GenotypeTransformer
+from models import GTTransformer
 from utils import map_sample_names_to_index, \
                                 map_genotype_encoding_to_index, \
                                 map_positional_encoding_to_index, \
@@ -23,9 +23,10 @@ def get_args():
     parser.add_argument('--sample_pos')
     return parser.parse_args()
 
+    
 def main():
     args = get_args()
-
+    
     # loading sample names (sample_name: sample_index)
     sample_names_index = map_sample_names_to_index(args.sample_names)
     # loading genotype encodings (sample_index: genotype_encoding)
@@ -35,22 +36,7 @@ def main():
     # splitting IDs into training, testing, and validating sets
     training_IDs, testing_IDs, validating_IDs = split_samples(args.sample_names, .8)
 
-
-    # Input Layer (Dense Layer)
-
-    # Positional Encoding Layer
-    # Encoder Layer
     
-    # # converting genotypes to tensors
-    # gt_tensors = get_tensors(sample_names_index,
-    #                    genotype_encodings_index,
-    #                    training_IDs)
-    # pos_tensors = get_tensors(sample_names_index,
-    #                    positional_encodings_index,
-    #                    training_IDs)
-    # print(len(gt_tensors))
 
 if __name__ == '__main__':
     main()
-
-
