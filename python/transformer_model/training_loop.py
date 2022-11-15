6ed565464a92cc04863df68882c69467eef5e89e
kristen-schneider <kristen.schneider@colorado.edu>
2022-11-14 17:01:57 -0800

    main code for training model

--- python/transformer_model/training_loop.py
+++ python/transformer_model/training_loop.py
@@ -9,10 +9,12 @@ import tensorflow as tf
 #from decoder import Decoder
 #from encoder import Encoder
 #from positional_encoding import PositionEmbeddingFixedWeights
+from models import GenotypeTransformer
 from utils import map_sample_names_to_index, \
                                 map_genotype_encoding_to_index, \
+                                map_positional_encoding_to_index, \
                                 split_samples, \
-                                get_genotype_tensors
+                                get_tensors
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -28,14 +30,25 @@ def main():
     sample_names_index = map_sample_names_to_index(args.sample_names)
     # loading genotype encodings (sample_index: genotype_encoding)
     genotype_encodings_index = map_genotype_encoding_to_index(args.sample_encodings)
+    # loading positional encodings (sample_index: positional_encoding)
+    positional_encodings_index = map_positional_encoding_to_index(args.sample_pos)
     # splitting IDs into training, testing, and validating sets
     training_IDs, testing_IDs, validating_IDs = split_samples(args.sample_names, .8)
 
-    # converting genotypes to tensors
-    gt_tensors = get_genotype_tensors(sample_names_index,
-                       genotype_encodings_index,
-                       training_IDs)
 
+    # Input Layer (Dense Layer)
+
+    # Positional Encoding Layer
+    # Encoder Layer
+    
+    # # converting genotypes to tensors
+    # gt_tensors = get_tensors(sample_names_index,
+    #                    genotype_encodings_index,
+    #                    training_IDs)
+    # pos_tensors = get_tensors(sample_names_index,
+    #                    positional_encodings_index,
+    #                    training_IDs)
+    # print(len(gt_tensors))
 
 if __name__ == '__main__':
     main()
