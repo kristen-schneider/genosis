dc9a94e1fe114566dfa2ff4e1e79204b1700a976
kristen-schneider <kristen.schneider@colorado.edu>
2023-01-10 08:55:23 -0800

    adding new directory for example data

--- bash/run_scripts/run_slice.sh
+++ bash/run_scripts/run_slice.sh
@@ -3,6 +3,7 @@
 # path to directories
 src_dir="/home/sdp/precision-medicine/cpp/src/"
 include_dir="/home/sdp/precision-medicine/cpp/include/"
+include_lib="/home/sdp/precision-medicine/lib/htslib/"
 bin_dir="/home/sdp/precision-medicine/cpp/bin/"
 
 # path to config file
@@ -14,10 +15,10 @@ bin=$bin_dir"slice"
 g++ $src_dir"slice_vcf.cpp" \
 	$src_dir"read_config.cpp" \
 	-I $include_dir \
+	-I $include_lib \
 	-I $conda_dir"include/" \
 	-L $conda_dir"lib" \
-	-lhts \
 	-o $bin
 
 # execute
-$bin $config_file
+#$bin $config_file
