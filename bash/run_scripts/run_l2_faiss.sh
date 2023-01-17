5ea8de0d869062d1418797f925492b40ddbb3cfd
kristen-schneider <kristen.schneider@colorado.edu>
2023-01-17 09:41:02 -0800

    testing on example

--- bash/run_scripts/run_l2_faiss.sh
+++ bash/run_scripts/run_l2_faiss.sh
@@ -8,22 +8,16 @@ src_dir="/home/sdp/precision-medicine/cpp/src/"
 include_dir="/home/sdp/precision-medicine/cpp/include/"
 conda_dir="/home/sdp/miniconda3/envs/pm/"
 bin_dir="/home/sdp/precision-medicine/cpp/bin/"
-data_dir="/home/sdp/precision-medicine/data/ped_sim_data/GBR_large/"
+data_dir="/home/sdp/pmed-local/data/"
 
 # path to encoded and query file
-all_samples=$data_dir"sample_hap_IDs.txt"
+all_samples=$data_dir"/1KG/all_sample_IDs.txt"
 test_samples=$data_dir"query_IDs.txt"
 train_samples=$data_dir"database_IDs.txt"
 k=20 # number of nearest neighbors to report
 delim="space"
 
-source ~/miniconda3/etc/profile.d/mamba.sh 
-mamba activate pm
-
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$conda_dir"lib"
-echo $LD_LIBRARY_PATH
 echo "Compiling..."
-
 
 bin=$bin_dir"faiss_l2"
 g++ $src_dir"faiss_l2.cpp" \
@@ -37,16 +31,16 @@ g++ $src_dir"faiss_l2.cpp" \
 	-lfaiss \
 	-o $bin
 
-echo "Executing..."
-for encoded_f in $data_dir"segments/"*.norm
-do
-        filename=$(basename -- $encoded_f)
-        base=${filename%.*}
-        echo "Running FAISS on" $filename
-        faiss_out=$data_dir"segments/"$base".norm.faissl2"
-        #echo $faiss_out
-        $bin $all_samples $encoded_f $all_samples $encoded_f $k $delim > $faiss_out
-done
+#echo "Executing..."
+#for encoded_f in $data_dir"segments/"*.norm
+#do
+#        filename=$(basename -- $encoded_f)
+#        base=${filename%.*}
+#        echo "Running FAISS on" $filename
+#        faiss_out=$data_dir"segments/"$base".norm.faissl2"
+#        #echo $faiss_out
+#        $bin $all_samples $encoded_f $all_samples $encoded_f $k $delim > $faiss_out
+#done
 
 
 #$bin $train_samples $encoded_file $train_samples $encoded_file $k $delim
