e9121d36117ea30f84fac8d92b2c7911942b9147
murad <mchowdh200@gmail.com>
2023-03-02 09:57:46 -0700

    add sbatch directives

--- pytorch/pairings_pipeline.sh
+++ pytorch/pairings_pipeline.sh
@@ -1,2 +1,11 @@
 #!/bin/env bash
-snakemake -s exploration_pipeline.smk -j 4 #--use-conda --conda-frontend mamba
+#SBATCH -j gt_similarity_search
+#SBATCH -p amilan
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --mem=64G
+#SBATCH --time=2:00:00 # hrs:min:sec
+#SBATCH --output=log/pairings_pipeline_%j.log
+#SBATCH --error=log/pairings_pipeline_%j.err
+
+snakemake -s exploration_pipeline.smk -j 4 --use-conda --conda-frontend mamba
