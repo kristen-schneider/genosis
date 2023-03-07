74be274d3d34933736b76a1a0f747db254a3fbac
murad <mchowdh200@gmail.com>
2023-03-07 08:20:54 -0700

    dont need the progress bar written to a log

--- pytorch/pairings_pipeline.sh
+++ pytorch/pairings_pipeline.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=32
 #SBATCH --mem=64G
 #SBATCH --time=2:00:00 # hrs:min:sec
-#SBATCH --output=log/pairings_pipeline_%j.log
+#SBATCH --output=/dev/null
 #SBATCH --error=log/pairings_pipeline_%j.err
 
 snakemake -s exploration_pipeline.smk -j 4 --use-conda --conda-frontend mamba
