4e84dc2c5657a3cd14d3f8c67e4433ce9635c093
kristen-schneider <kristen.schneider@colorado.edu>
2023-03-15 09:58:23 -0700

    removed old modle

--- python/old_model/training_loop/run.sh
+++
@@ -1,7 +0,0 @@
-python create_vectors.py \
-    --sample-list ~/precision-medicine/data/samples/chr8-30x/testing.samples \
-    --model-path base_model.h5 \
-    --output-path ~/precision-medicine/data/segments/chr8-30x/chr8-30x.seg.0.embeddings \
-    --batch-size 32 \
-    --sample_id_filename ~/precision-medicine/data/samples/chr8-30x/all.samples \
-    --genotype_filename ~/precision-medicine/data/segments/chr8-30x/chr8-30x.seg.0.encoded
