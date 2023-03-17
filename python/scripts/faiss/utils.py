7d9ebdd2f7c8b7ff05cb46b367ad6dc35b1ed9e7
kristen-schneider <kristen.schneider@colorado.edu>
2023-03-17 13:17:35 -0700

    removing unused scripts

--- python/scripts/faiss/utils.py
+++
@@ -1,9 +0,0 @@
-def sum_distances(distances):
-    penalty = 100
-    sum = 0
-    for d in distances:
-        try:
-            sum += d
-        except TypeError:
-            sum += penalty
-    return sum
