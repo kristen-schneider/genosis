d3e714a47b1a16b3c3f4cf9413249a0241bb3524
kristen-schneider <kristen.schneider@colorado.edu>
2023-05-10 14:18:21 -0700

    cleaning up python files

--- python/scripts/distance/read_encoding.py
+++
@@ -1,9 +0,0 @@
-def read_encoding_file(encoding_file):
-    encodings = dict()
-    f = open(encoding_file, 'r')
-    for line in f:
-        L = line.strip().split()
-        sample_ID = L[0]
-        encodings[sample_ID] = [float(i) for i in L[1:]]
-    f.close()
-    return encodings
---
+++ python/scripts/faiss/split_embeddings.py
@@ -0,0 +1,33 @@
+import argparse
+import numpy as np
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--emb_dir', type=str)
+    parser.add_argument('--all_emb', type=str)    
+    return parser.parse_args()
+
+
+def main():
+    # reading embedding file with all segments
+    # and write out to segment files
+    args = parse_args()
+    emb_dir = args.emb_dir
+    all_emb = args.all_emb
+    read_embedding_file(emb_dir, all_emb)
+
+def read_embedding_file(embeddings_dir, embeddings_all):
+    with open(embeddings_all, 'r') as f:
+        for line in f:
+            L = line.strip().split()
+            sample = L[0]
+            segment = L[1]
+            embedding = L[2:]
+            # append sample and embedding to segment file
+            segment_file = embeddings_dir + 'segment.' + segment + '.idx'
+            with open(segment_file, 'a') as f:
+                f.write(sample + ' ' + ' '.join(embedding) + '\n')
+
+
+if __name__ == '__main__':
+    main()
