3d7a85c0f37002c3f98863a44d255ca46b305856
kristen-schneider <kristen.schneider@colorado.edu>
2023-05-15 14:30:57 -0700

    adding phased ibd

--- python/scripts/ibd/phased_ibd.py
+++ python/scripts/ibd/phased_ibd.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from IPython.display import display
+#from IPython.display import display
 import phasedibd as ibd
 import argparse
 
@@ -7,6 +7,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--vcf')
     parser.add_argument('--map')
+    parser.add_argumnet('--out')
     return parser.parse_args()
 
 def main():
@@ -14,13 +15,14 @@ def main():
     
     print('vcf: ', args.vcf)
     print('map: ', args.map)
+    print('out: ', args.out)
     
     #haplotypes = ibd.VcfHaplotypeAlignment(args.vcf)
     haplotypes = ibd.VcfHaplotypeAlignment(args.vcf, args.map)
     tpbwt = ibd.TPBWTAnalysis()
     ibd_results = tpbwt.compute_ibd(haplotypes)
     display(ibd_results)
-    ibd_results.to_csv('~/precision-medicine/data/ibd/phasedibd/chr8.30x.pibd.csv', sep='\t')
+    ibd_results.to_csv(args.out, sep='\t')
 
 if __name__ == '__main__':
     main()
