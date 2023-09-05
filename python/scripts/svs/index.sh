b8e0f197e62253584bd8bb2b7ab00022a39250f8
kristen-schneider <kristen.schneider@colorado.edu>
2023-09-05 16:38:21 -0600

    adding variables to bash scripts

--- python/scripts/svs/index.sh
+++ python/scripts/svs/index.sh
@@ -1,10 +1,27 @@
+#!/usr/bin/env bash
+
+#SBATCH -p short
+#SBATCH --job-name=svs-index
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=64gb
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=krsc0813@colorado.edu
+#SBATCH --output=/Users/krsc0813/precision-medicine/python/scripts/svs/svs-index.out
+#SBATCH --error=/Users/krsc0813/precision-medicine/python/scripts/svs/svs-index.err
+
 echo "1. building svs index slices..."
 start_index=$(date +%s.%3N)
 
+emb_dir="/Users/krsc0813/precision-medicine/example/embeddings/"
+idx_dir="/Users/krsc0813/precision-medicine/example/svs_index/"
+ids="/Users/krsc0813/precision-medicine/example/query_hap_IDs.txt"
+
 python build_svs_index.py \
-	--emb_dir /home/sdp/pmed-local/data/chrm10-12_embeddings/ \
-	--idx_dir /home/sdp/pmed-local/data/chrm10-12_svs_index/ \
-	--db_samples /home/sdp/pmed-local/data/1kG_hap_IDs.txt \
+	--emb_dir $emb_dir \
+	--idx_dir $idx_dir \
+	--db_samples $ids \
 	--emb_ext emb
 
 end_index=$(date +%s.%3N)
--- python/scripts/svs/search.sh
+++ python/scripts/svs/search.sh
@@ -1,14 +1,33 @@
+#!/usr/bin/env bash
+
+#SBATCH -p short
+#SBATCH --job-name=svs-search
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=64gb
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=krsc0813@colorado.edu
+#SBATCH --output=/Users/krsc0813/precision-medicine/python/scripts/svs/svs-search.out
+#SBATCH --error=/Users/krsc0813/precision-medicine/python/scripts/svs/svs-search.err
+
+
 echo "searching svs index slices..."
 start_search=$(date +%s.%3N)
 
+emb_dir="/Users/krsc0813/precision-medicine/example/embeddings/"
+idx_dir="/Users/krsc0813/precision-medicine/example/svs_index/"
+res_dir="/Users/krsc0813/precision-medicine/example/svs_results/"
+ids="/Users/krsc0813/precision-medicine/example/query_hap_IDs.txt"
+
 python search_svs_index.py \
-	--idx_dir /home/sdp/pmed-local/data/chrm10-12_svs_index/ \
-	--emb_dir /home/sdp/pmed-local/data/chrm10-12_embeddings/ \
+	--idx_dir $idx_dir \
+	--emb_dir $emb_dir \
 	--emb_ext emb \
-	--db_samples /home/sdp/pmed-local/data/1kG_hap_IDs.txt \
-	--q_samples /home/sdp/pmed-local/data/1kG_hap_IDs.txt \
+	--db_samples $ids \
+	--q_samples $ids \
 	--k 20 \
-	--out_dir /home/sdp/pmed-local/data/chrm10-12_svs_results/
+	--out_dir $res_dir
 
 end_search=$(date +%s.%3N)
 search_time=$(echo "scale=3; $end_search - $start_search" | bc)
