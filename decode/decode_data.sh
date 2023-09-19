90d04b2ee3b5b948e7bb5b991a89781add4afb60
kristen-schneider <kristen.schneider@colorado.edu>
2023-09-19 02:45:13 -0700

    added remove bad id to data gen

--- decode/decode_data.sh
+++ decode/decode_data.sh
@@ -5,9 +5,12 @@ set -e pipefail
 ### TODO: 
 ### modify these paths
 
+python_src="/path/to/python_scripts/"
 out_dir="/path/to/out_dir/"
 
-samples="/path/to/sample_list.pns"
+my_samples="/path/to/my_samples.pns"
+all_samples="/path/to/all_sample.pns"
+good_samples="/path/to/avail_samples.pns"
 chi="/path/to/chi.chi"
 out_vcf=$out_dir"out.vcf"
 
@@ -19,11 +22,24 @@ out_map=$out_dir"out.map"
 ###
 ###
 
+# remove bad ids from samples file
+echo "1. Removing bad IDs from "$my_samples
+python $python_src"decode/remove_bad_IDs.py" \
+ --my_samples $my_samples \
+ --all_samples $all_samples \
+ --out_sample $good_samples
+
 # make VCF file
-chitools view -p $samples $chi -f vcf > $out_vcf
+echo "2. Preparing a VCF file."
+chitools view -p $good_samples $chi -f vcf > $out_vcf
 bgzip $out_vcf
 tabix -p vcf $out_vcf".gz"
 
-# make map file
+# make MAP file
+echo "3. Preparing a MAP file."
 awk '{ print $1 $2 $7 $3}' $full_map > $out_map
 #python $cm_py $full_map $cmap
+
+# makde PED file
+echo "4. Preparing a PED file."
+
