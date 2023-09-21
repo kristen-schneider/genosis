1f005dbf3aa35d9848d5f2c53c01d10b6945cea3
kristen-schneider <kristen.schneider@colorado.edu>
2023-09-21 01:03:50 -0700

    aggregating sample summaries and realtionships and plotting viloin plot for two metrics

--- python/scripts/decode/lump_relations.py
+++ python/scripts/decode/lump_relations.py
@@ -1,4 +1,6 @@
 import argparse
+import plot_decode as pd
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Label pedigree relations between individuals')
     parser.add_argument('--relations', type=str, help='relations file')
@@ -12,29 +14,46 @@ def main():
     samples_file = args.samples
     data_dir = args.data_dir
     
-    all_relations_scores = {}
-    
     samples = read_samples(samples_file)
     pair_relations = read_relations(relations_file)
 
-def get_relation_scores(samples, pair_relations, data_dir, all_relations_scores):
+    all_relations_scores_pop = get_relation_scores(samples, pair_relations, data_dir, ext='.pop')
+    all_relations_scores_ibd = get_relation_scores(samples, pair_relations, data_dir, ext='.ibd')
+    
+    pd.violin_plot(all_relations_scores, data_dir + 'POP.png')
+    pd.violin_plot(all_relations_scores, data_dir + 'IBD.png')
+	
+def get_relation_scores(samples, pair_relations, data_dir, ext):
+   
+    all_relations_scores = {}
+
     for sample in samples:
         print(sample)
         # open sample file
-        sample_file = data_dir + '/' + sample + '.csv'
+        sample_file = data_dir + sample + ext + '.csv'
         query = sample
         with open(sample_file, 'r') as file:
+            # combine any sample amtches across haps
+            sample_haps = {}
             for line in file:
                 if ":" not in line:
-                    break
-                values = line.strip().split()
-                match = values[0].split(":")[0]
-                score = float(values[1])
-                relation = pair_relations[query][match]
-                try:
-                    all_relations_scores[relation].append(score)
-                except KeyError:
-                    all_relations_scores[relation] = [score]
+                    hap = line
+                else:
+                    values = line.strip().split()
+                    match_ID = values[0].split(":")[0]
+                    score = float(values[1])
+                    try:
+                        sample_haps[match_ID] += score
+                    except KeyError:
+                        sample_haps[match_ID] = score
+        # for all matches with this sample append and get score
+        for m in sample_haps:
+            curr_score = sample_haps[m]
+            relation = pair_relations[query][m]
+            try:
+                all_relations_scores[relation].append(curr_score)
+            except KeyError:
+                all_relations_scores[relation] = [curr_score]
             
 def read_relations(relations_file):
     relations = {}
@@ -55,4 +74,4 @@ def read_samples(samples_file):
     return samples
     
 if __name__ == '__main__':
-    main()
+	    main()
