750487f9ac6127000d49c6095026da1343553ed3
kristen-schneider <kristen.schneider@colorado.edu>
2023-10-16 12:46:49 -0600

    adding ancestry eval

--- python/scripts/evaluate/label_relationship.py
+++ python/scripts/evaluate/label_relationship.py
@@ -4,6 +4,7 @@ import plot_decode as pd
 def parse_args():
     parser = argparse.ArgumentParser(description='Label pedigree relations between individuals')
     parser.add_argument('--relations', type=str, help='relations file')
+    parser.add_argument('--ancestry', type=str, help='ancestry file')
     parser.add_argument('--samples', type=str, help='samples_file')
     parser.add_argument('--data_dir', type=str, help='data directory for out csv files')
     return parser.parse_args()
@@ -11,24 +12,41 @@ def parse_args():
 def main():
     args = parse_args()
     relations_file = args.relations
+    ancestry_file = args.ancestry
     samples_file = args.samples
     data_dir = args.data_dir
-    
+   
+     
     samples = read_samples(samples_file)
-    pair_relations = read_relations(relations_file)
+    
+    # if peidgree --> relations file
+    if ancestry_file == 'None':
+        print("evaluating pedigree data...")
 
-    all_relations_scores_svs = get_relation_scores(samples, pair_relations, data_dir, ext='.svs')
-    all_relations_scores_pop = get_relation_scores(samples, pair_relations, data_dir, ext='.pop')
-    all_relations_scores_ibd = get_relation_scores(samples, pair_relations, data_dir, ext='.ibd')
+        pair_relations = read_relations(relations_file)
 
-    write_relations_scores(all_relations_scores_svs, data_dir + '/SVS.results')    
-    write_relations_scores(all_relations_scores_pop, data_dir + '/POP.results')    
-    write_relations_scores(all_relations_scores_ibd, data_dir + '/IBD.results')    
+        all_relations_scores_svs = get_relation_scores(samples, pair_relations, data_dir, ext='.svs')
+        all_relations_scores_pop = get_relation_scores(samples, pair_relations, data_dir, ext='.pop')
+        all_relations_scores_ibd = get_relation_scores(samples, pair_relations, data_dir, ext='.ibd')
 
-    pd.violin_plot(all_relations_scores_svs, "SVS scores", data_dir + '/SVS.png')
-    pd.violin_plot(all_relations_scores_pop, "popcount", data_dir + '/POP.png')
-    pd.violin_plot(all_relations_scores_ibd, "merge-gap", data_dir + '/IBD.png')
+        write_relations_scores(all_relations_scores_svs, data_dir + '/SVS.results')    
+        write_relations_scores(all_relations_scores_pop, data_dir + '/POP.results')    
+        write_relations_scores(all_relations_scores_ibd, data_dir + '/IBD.results')    
+
+        pd.violin_plot(all_relations_scores_svs, "SVS scores", data_dir + '/SVS.png')
+        pd.violin_plot(all_relations_scores_pop, "popcount", data_dir + '/POP.png')
+        pd.violin_plot(all_relations_scores_ibd, "merge-gap", data_dir + '/IBD.png')
 	
+    # else --> ancestry file    
+    else:
+        print("evaluating ancestry data...")
+        super_pops, sub_pops = 
+        super_labels, sub_labels = 
+        
+        super_ancestry, sub_ancestry = label_acestry(ancestry_file)
+        
+
+
 def get_relation_scores(samples, pair_relations, data_dir, ext):
    
     all_relations_scores = {}
@@ -80,12 +98,21 @@ def read_relations(relations_file):
                 relations[i1] = {i2: relation}
     return relations
     
-def read_samples(samples_file):
-    samples = []
-    with open(samples_file, 'r') as file:
+def label_ancestry(ancestry_file):
+    super_ancestry = {}
+    sub_ancestry = {}
+
+    with open(ancestry_file, 'r') as file:
         for line in file:
-            samples.append(line.strip())
-    return samples
+            values = line.strip().split(',')
+            sample_ID = values[0]
+            sub_pop = values[3]
+            super_pop = values[5]
+                   
+            super_ancestry[sample_ID] = super_pop
+            sub_ancestry[sample_ID] = sub_pop
+
+    return super_ancestry, sub_ancestry
     
 def write_relations_scores(all_relations_scores, out_f):
     o = open(out_f, 'w')
@@ -93,6 +120,13 @@ def write_relations_scores(all_relations_scores, out_f):
         o.write(relation + ',' + str(all_relations_scores[relation]))
         o.write('\n')
     o.close()
+
+def read_samples(samples_file):
+    samples = []
+    with open(samples_file, 'r') as file:
+        for line in file:
+            samples.append(line.strip())
+    return samples
 
 if __name__ == '__main__':
     main()
