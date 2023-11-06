171711256840ee0cc8a02ee81c97161f3e4250aa
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-06 12:48:50 -0700

    modifying working directory

--- example/clean.sh
+++ example/clean.sh
@@ -1,8 +1,13 @@
-rm *.txt
-rm *.out
+example_dir="/Users/krsc0813/precision-medicine/example/"
+cd $example_dir
+
 rm pipeline.log
+rm sample_IDs.txt
+rm vcf_bp.txt
 rm segment_boundary.map
 rm interpolated.map
+rm zeros.out
+rm *hap_IDs.txt
 
 rm -r log/
 rm -r benchmark/
