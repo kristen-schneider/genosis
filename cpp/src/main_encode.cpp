55a215a80c631d9380cc2e239a737e357b16f6e1
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-06 15:48:49 -0700

    better file naming

--- cpp/src/main_encode.cpp
+++ cpp/src/main_encode.cpp
@@ -40,44 +40,12 @@ int main(int argc, char* argv[]){
 	// name output files
 	string output_gt_file = encoding_dir + "chrm" + to_string(chrm_idx) + ".segment" + to_string(segment_idx) + ".gt";
 	string output_pos_file = encoding_dir + "chrm" + to_string(chrm_idx) + ".segment" + to_string(segment_idx) + ".pos";
-	cout << "output gt file: " << output_gt_file << endl;
-	cout << "output pos file: " << output_pos_file << endl;
-    /*
-    vector<string> file_path;
-	string path_delim = "/";
-	size_t pos = 0;
-	string path_token;
-	while ((pos = vcf_segment_file.find(path_delim)) != std::string::npos){
-		path_token = vcf_segment_file.substr(0, pos);
-		file_path.push_back(path_token);
-		vcf_segment_file.erase(0, pos + path_delim.length());
-	}
-	path_token = vcf_segment_file.substr(0, pos);
-	
-        file_path.push_back(path_token);
-	// split the file name and get the chrm idx and seg idx
-	string file_name = file_path.back();
-	vector<string> file_name_vec;
-	string file_delim = ".";
-	pos = 0;
-	string file_token;
-	while ((pos = file_name.find(file_delim)) != std::string::npos){
-                file_token = file_name.substr(0, pos);
-                file_name_vec.push_back(file_token);
-                file_name.erase(0, pos + file_delim.length());
-        }
-        file_token = file_name.substr(0, pos);
-        file_name_vec.push_back(file_token);
-	int chrm_idx = stoi(file_name_vec[0].erase(0, 4));      // chromosome idx
-	int segment_idx = stoi(file_name_vec[1].erase(0, 7));   // segment idx
-	*/
-
 	
 	// 2. make gt encoding map
 	// format: (gt (string): encoding <int, int>)
 	map<string, vector<int> > encoding_def_map = map_gt_encoding(encoding_def_file);	
 	
-        // 3. make cm positional encoding map
+    // 3. make cm positional encoding map
 	// format: (chrm (int): bp_cm <int, float>)
 	map<int, map<int, float> > chrm_bp_cm_map = map_bp_cm(interpolated_map);
 	
