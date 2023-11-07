9992f370db487fe94761da1c6eaf98c076be6099
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-07 15:09:03 -0700

    removing extra scripts

--- helper_scripts/plink_similarity.sh
+++ helper_scripts/plink_similarity.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition=short
 #SBATCH --job-name=plink-benchmark
-#SBATCH --output=/Users/krsc0813/1KG_data/chr1_22/out/plink-benchmark.out
-#SBATCH --error=/Users/krsc0813/1KG_data/chr1_22/err/plink-benchmark.err
+#SBATCH --output=/Users/krsc0813/1KG_data/chrm15/out/plink-benchmark.out
+#SBATCH --error=/Users/krsc0813/1KG_data/chrm15/err/plink-benchmark.err
 #SBATCH --time=0-23:00:00
 #SBATCH --qos=normal
 #SBATCH --nodes=1
@@ -16,14 +16,14 @@
 # plink2 --make-king-table
 
 # TODO: fill in file paths appropriately.
-vcf="/Users/krsc0813/1KG_data/chr1_22/chrm_1-22.vcf.gz"
-data_dir="/Users/krsc0813/1KG_data/chr1_22"
+vcf="/Users/krsc0813/1KG_data/1kg_30x_phased/1kGP_high_coverage_Illumina.chr2.filtered.SNV_INDEL_SV_phased_panel.vcf.gz"
+data_dir="/Users/krsc0813/1KG_data/chrm15/"
 
 cd $data_dir
 echo "plink --genome"
 plink --vcf $vcf \
        	--genome \
-	--out $data_dir"/plink-genome"
+	--out $data_dir"/plink-genome/chrm15"
 
 echo "plink2 --kinship"
 plink2 --vcf $vcf --make-king-table
