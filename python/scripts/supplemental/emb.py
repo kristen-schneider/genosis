9384a39a73e0d82d2f5e48d707f45a421b7401d1
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-13 11:34:11 -0700

    scripts for supplemental figure

---
+++ python/scripts/supplemental/emb.py
@@ -0,0 +1,17 @@
+import argparse
+import matplotlib.pytplot as plt
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--boundary_file')
+
+def main():
+
+def count_cm():
+    """
+    
+    """
+
+
+if __name__ == '__main__':
+    main()
