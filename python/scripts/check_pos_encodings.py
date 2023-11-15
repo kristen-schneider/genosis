60d73b69c543e6fd21e61f5fc9935a95cdf43d1e
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-15 16:09:48 -0700

    taking one file at a time rather than a whole directory

--- python/scripts/check_pos_encodings.py
+++ python/scripts/check_pos_encodings.py
@@ -1,44 +1,32 @@
 import os
 import argparse
 
-
 MINIMUM=7   # minimum length for an encoding vector for the segment to be included
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--pos_dir', type=str, help='directory of positional encoding vectors')
-    parser.add_argument('--pos_ext', type=str, default='.pos', help='positional encoding vector extension')
+    parser.add_argument('--pos_enc', type=str, help='genotype encoding file')
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    pos_dir = args.pos_dir
-    pos_ext = args.pos_ext
-    
-    # look for files with positional encoding extension
-    for pos_file in os.listdir(pos_dir):
-        print(pos_file)
-        if pos_file.endswith(pos_ext):
-            pos_encoding = pos_dir + pos_file
-            check_for_empty_entries(pos_encoding)
+    pos_encoding_file = args.pos_enc
+    check_for_empty_entries(pos_encoding_file)
 
-def check_for_empty_entries(pos_encoding):
+def check_for_empty_entries(pos_encoding_file):
     """
     check for segments with encoding vectors less than MINIMUM
     """
     #empty = False   # flag if positional encoding less than MINIMUM is found
-    p = open(pos_encoding, 'r')
+    p = open(pos_encoding_file, 'r')
+    print(pos_encoding_file)
     for line in p:
         L = line.strip().split()
         # if len of positional encoding vector is less than MINIMUM:
         if len(L) < MINIMUM:
-            print('!! Removing', pos_encoding)
-            os.remove(pos_encoding) 
-            #empty = True
+            print('!! Removing', pos_encoding_file)
+            os.remove(pos_encoding_file) 
             break       # exit loop as soon as bad encoding is found. don't keep searching file
-    ## remove a segment file if there are bad encodings (less than MINIMUM)
-    #if empty:
-    #    print('!! Removing', pos_encoding)
-    #    os.remove(pos_encoding) 
+
 if __name__ == "__main__":
     main()
