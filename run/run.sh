a54129f61d1ac5aa4e78d65fc5080139c62d5894
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-15 12:14:46 -0700

    adding variables in run script to pass to index, search, evaluate

--- run/run.sh
+++ run/run.sh
@@ -18,18 +18,31 @@ set -e pipefail
 
 ### TODO: 
 ### modify these paths
-
+# scripts
 index_script="./run/index.sh"
 search_script="./run/search.sh"
-
+evaluate_script="./run/evaluate.sh"
+# directories
+pmed_dir="./"
+out_dir="./example/"
+# config file
+config_file="./example/example.yml"
 ###
 ###
 
 # make log directory
+cd $out_dir
 mkdir log
+
+# move to project dir
+cd $pmed_dir
+
 # run indexing step
 echo "Running indexing step."
-bash $index_script
+bash $index_script $pmed_dir $out_dir $config_file
 # run searching step
 echo "Running searching step."
-bash $search_script
+bash $search_script $pmed_dir $out_dir $config_file
+# run evaluation step
+echo "Running evaluation step."
+bash $evaluate_script $pmed_dir $out_dir $config_file
--- run/search.sh
+++ run/search.sh
@@ -2,15 +2,9 @@
 
 set -e pipefail
 
-### TODO: 
-### modify these paths
-
-pmed_dir="./"
-out_dir="./example/"
-config=$out_dir"example.yml"
-
-###
-###
+pmed_dir=$1
+out_dir=$2
+config=$3
 
 ## These directories should be correct.
 ## If you have changed where scripts exist, change these paths
@@ -19,9 +13,6 @@ log=$out_dir"log/search.log"
 ##
 ##
 
-# go to project directory
-cd $pmed_dir
-
 # for singularity container
 #. /opt/conda/etc/profile.d/conda.sh
 # load conda and activate snakemake env for run
@@ -29,12 +20,15 @@ cd $pmed_dir
 #.~/miniconda3/bin/conda
 #conda_dir="/home/sdp/miniconda3/envs/"
 #source ~/miniconda3/etc/profile.d/mamba.sh 
-echo $SHELL
-source  ~/.bashrc
 #export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/miniconda3/condabin/conda
 #conda activate snakemake
 
-# run pipeline
+# activate conda / mamba
+source  ~/.bashrc
+# go to project directory
+cd $pmed_dir
+
+# run searching pipeline
 # 5. search slices
 echo "5. searching index slices..." >> $log
 start_search=$(date +%s.%3N)
@@ -43,7 +37,6 @@ snakemake \
     -c 16 \
     -j 10 \
     --configfile=$config \
-    --rerun-incomplete
 end_search=$(date +%s.%3N)
 search_time=$(echo "scale=3; $end_search - $start_search" | bc)
 echo "--SEARCH: $search_time seconds" >> $log
@@ -56,10 +49,7 @@ snakemake \
     -s $smk_dir"AGGREGATE.smk" \
     -c 16 \
     -j 10 \
-    --use-conda \
-    --conda-frontend mamba \
     --configfile=$config \
-    --rerun-incomplete
 end_aggregate=$(date +%s.%3N)
 aggregate_time=$(echo "scale=3; $end_aggregate - $start_aggregate" | bc)
 echo "--AGGREGATE: $aggregate_time seconds" >> $log
