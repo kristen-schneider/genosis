c64c5708c02dca4b5cb87cefd8aa9d0443a22e01
kristen-schneider <kristen.schneider@colorado.edu>
2023-11-20 17:12:53 -0700

    added a few more plot generations to plot.sh

--- helper_scripts/plot.sh
+++ helper_scripts/plot.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #SBATCH --partition=short
 #SBATCH --job-name=plot
 #SBATCH --output=./out/plot.out
@@ -11,12 +10,51 @@
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=krsc0813@colorado.edu
 
-plot_script="/Users/krsc0813/biobagg_analysis/plotting/evaluate_ancestry.py"
+biobagss_dir="/Users/krsc0813/biobagg_analysis/"
 pop_file="/Users/krsc0813/1KG_data/samples.ancestry"
-knn_file="/scratch/Users/krsc0813/chr1_22/TOP_HITS.txt"
-png_dir="/scratch/Users/krsc0813/chr1_22/violin_plots/"
+trio_file="/Users/krsc0813/1KG_data/samples.trios"
+out_dir="/scratch/Users/krsc0813/chr1_22/"
+knn_file=$out_dir"TOP_HITS.txt"
 
-python $plot_script \
-    --pop $pop_file\
-    --knn $knn_file\
-    --png $png_dir
+python $biobagss_dir"plotting/evaluate_ancestry" \
+  --pop $pop_file\
+  --knn $knn_file\
+  --png $out_dir"violin_plots/"
+
+python $biobagss_dir"plotting/search_time.py" \
+  --num_samples 3202 \
+  --in_file $out_dir"log/search.log" \
+  --out_file $out_dir"time_plots/1kg_search_time_histo.png" \
+  --height 4 \
+  --width 8
+
+python $biobagss_dir"plotting/top_hits_umap.py" \
+  --in_file $knn_file \
+  --label_file $pop_file \
+  --out_file $out_dir"other_plots/top_hit_umap.png"
+
+python plotting/top_hits_pca.py \
+  --in_file $knn_file \
+  --label_file $pop_file \
+  --out_file $out_dir"other_plots/top_hit_pca.png"
+
+python plotting/plot_topk_score_distro.py \
+  --topk_file $knn_file \
+  --label_file $pop_file \
+  --out_file $out_dir"other_plots/topk_score_distro.png"  \
+  --height 2 \
+  --width 8 \
+  --bins 100 \
+  --ped_file $trio_file
+
+python plotting/plot_topk_score_distro.py \
+  --topk_file $knn_file \
+  --label_file $pop_file \
+  --out_file $out_dir"other_plots/topk_score_distro.log.png"  \
+  --height 2 \
+  --width 8 \
+  --bins 100 \
+  --ped_file $trio_file \
+  --log
+
+
