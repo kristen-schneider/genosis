89a3484772fad9fae2cf7dbebf6b92a136006444
kristen-schneider <kristen.schneider@colorado.edu>
2024-04-05 11:04:20 -0600

    running example

--- run/run.sh
+++ run/run.sh
@@ -11,38 +11,40 @@
 #SBATCH --time=23:00:00
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=name@email.com
-#SBATCH --output=/Users/krsc0813/precision-medicine/example/log/example.out
-#SBATCH --error=/Users/krsc0813/precision-medicine/example/err/example.err
+#SBATCH --output=example/log/example.out
+#SBATCH --error=example/err/example.err
 
 set -e pipefail
 
 ### TODO: 
 ### modify these paths
-# scripts
-index_script="/Users/krsc0813/precision-medicine/run/index.sh"
-search_script="/Users/krsc0813/precision-medicine/run/search.sh"
-# directories
+# the gess repo
 gess_dir="/Users/krsc0813/precision-medicine/"
-out_dir="/Users/krsc0813/precision-medicine/example/"
-# config file
-config_file=$out_dir"example.yml"
+# the local directory for output and config files
+local_dir="/Users/krsc0813/precision-medicine/example/"
+
+### scripts
+index_script=$gess_dir"run/index.sh"
+search_script=$gess_dir"run/search.sh"
+### config file
+config_file=$local_dir"example.yml"
 ###
 ###
 
 # make log directory
-test ! -d $out_dir"log/" && mkdir $out_dir"log/"
+test ! -d $local_dir"log/" && mkdir $local_dir"log/"
 
 # move to project dir
 cd $gess_dir
 
 # run indexing step
 echo "Running indexing step."
-bash $index_script $gess_dir $out_dir $config_file
+bash $index_script $gess_dir $local_dir $config_file
 # run searching step
 wait
 
 echo "Running searching step."
-bash $search_script $gess_dir $out_dir $config_file
+bash $search_script $gess_dir $local_dir $config_file
 #wait
 
 ## run evaluation step
